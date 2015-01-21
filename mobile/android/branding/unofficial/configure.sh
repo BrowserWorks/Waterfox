ebc70d80217174be41758530b078efecc1029d41
Andy Balholm <andy@balholm.com>
2015-01-21 14:46:57 -0800

    Change name to "Compass Browser".

--- mobile/android/branding/unofficial/configure.sh
+++ mobile/android/branding/unofficial/configure.sh
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ANDROID_PACKAGE_NAME=org.mozilla.fennec_`echo $USER | sed 's/-/_/g'`
-MOZ_APP_DISPLAYNAME="Fennec `echo $USER | sed 's/-/_/g'`"
+ANDROID_PACKAGE_NAME=io.compassfoundation.fennec
+MOZ_APP_DISPLAYNAME="Compass Browser"
 MOZ_UPDATER=
 MOZ_ANDROID_ANR_REPORTER=
