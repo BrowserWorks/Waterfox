332939b887b6d49028a72b44f1a0b778fa6917ed
Alex Kontos <alex@waterfoxproject.org>
2016-11-17 19:04:37 +0000

    Waterfox branding

---
+++ browser/branding/waterfox/configure.sh
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_DISPLAYNAME=Waterfox
+
+# Hard code profile locations due to rebranding changing the default location
+if test "$OS_ARCH" = "WINNT"; then
+  MOZ_APP_PROFILE=Mozilla/Firefox
+elif test "$OS_ARCH" = "Linux"; then
+  MOZ_APP_PROFILE=mozilla/Firefox
+elif test "$OS_ARCH" = "Darwin"; then
+  MOZ_APP_PROFILE=Firefox
+fi
