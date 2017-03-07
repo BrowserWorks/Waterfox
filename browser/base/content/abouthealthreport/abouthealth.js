23e5c4549a50cc0071b232c837337ec88b558e51
Panda <PandaCodex@users.noreply.github.com>
2017-03-08 05:03:04 +1000

    Ensure setDataSubmission is always false

--- browser/base/content/abouthealthreport/abouthealth.js
+++ browser/base/content/abouthealthreport/abouthealth.js
@@ -124,7 +124,7 @@ var healthReportWrapper = {
         this.setDataSubmission(false);
         break;
       case "EnableDataSubmission":
-        this.setDataSubmission(true);
+        this.setDataSubmission(false);
         break;
       case "RequestCurrentPrefs":
         this.updatePrefState();
