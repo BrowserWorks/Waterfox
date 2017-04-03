92d9f2394d0ff996b2baa632758b2b311d166625
Alex Kontos <alex@waterfoxproject.org>
2017-04-03 14:05:50 +0100

    Add Waterfox's Branding

--- browser/branding/official/configure.sh
+++ browser/branding/official/configure.sh
@@ -2,4 +2,22 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Firefox
+MOZ_APP_DISPLAYNAME=Waterfox
+
+# Hard code profile locations due to rebranding changing the default location
+if test "$OS_ARCH" = "WINNT"; then
+  MOZ_APP_PROFILE=Mozilla/Firefox
+elif test "$OS_ARCH" = "Linux"; then
+  MOZ_APP_PROFILE=mozilla/Firefox
+elif test "$OS_ARCH" = "Darwin"; then
+  MOZ_APP_PROFILE=Firefox
+fi
+
+MOZ_TELEMETRY_REPORTING=0
+MOZ_ADDON_SIGNING=0
+MOZ_REQUIRE_SIGNING=0
+MOZ_ADOBE_EME=0
+MOZ_WIDEVINE_EME=0
+MOZ_SERVICES_HEALTHREPORT=0
+MOZ_CRASHREPORTER=0
+MOZ_DATA_REPORTING=0
--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("startup.homepage_override_url", "");
-pref("startup.homepage_welcome_url", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/");
+pref("startup.homepage_override_url", "https://www.waterfoxproject.org/blog/waterfox-%VERSION%-release-download");
+pref("startup.homepage_welcome_url", "https://www.waterfoxproject.org/blog/waterfox-%VERSION%-release-download");
 pref("startup.homepage_welcome_url.additional", "");
 // Interval: Time between checks for a new version (in seconds)
 pref("app.update.interval", 43200); // 12 hours
@@ -15,10 +15,10 @@ pref("app.update.download.backgroundInterval", 0);
 pref("app.update.promptWaitTime", 691200);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
-pref("app.update.url.manual", "https://www.mozilla.org/firefox/");
+pref("app.update.url.manual", "https://www.waterfoxproject.org/downloads");
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard.
-pref("app.update.url.details", "https://www.mozilla.org/%LOCALE%/firefox/notes");
+pref("app.update.url.details", "https://www.waterfoxproject.org/blog/");
 
 // The number of days a binary is permitted to be old
 // without checking for an update.  This assumes that
