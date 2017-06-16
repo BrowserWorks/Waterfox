5b7b6594dc800d1ce60f0475c472c5645b29cb05
hawkeye116477 <hawkeye116477@gmail.com>
2017-06-16 21:34:58 +0200

    Add script for building KWaterfox and add patches for KWaterfox

---
+++ _Plasma_Build/build_kwaterfox.sh
@@ -0,0 +1,97 @@
+# Waterfox KDE Plasma Edition quick build script
+# Version: 1.0
+
+#!/bin/bash
+
+
+# Set current directory to Waterfox source code directory.
+SourceDir=$(cd "$(dirname "$0")/.." && pwd)
+cd $SourceDir
+
+VERSION=$(<$SourceDir/browser/config/version.txt)
+
+# Test internet connection
+function testConnection(){
+	echo "Checking for internet connection!"
+	wget -q --tries=10 --timeout=20 --spider https://ftp.mozilla.org
+	if [[ $? -eq 0 ]]; then
+		# 0 = true
+		echo "Internet connection found!"
+	    return 0 
+	else
+	    # 1 = false
+	    echo "No internet connection found!"
+	    return 1
+	    exit 0
+	fi
+}
+
+
+# Apply KDE Plasma patch to locally downloaded repository.
+function ApplyKDE(){
+
+	# Download patch if not exist and replace some words
+	if [ ! -f "$SourceDir/_Plasma_Build/mozilla-kde-$VERSION.patch" ] && [ ! -f "$SourceDir/_Plasma_Build/firefox-kde-$VERSION.patch" ]; then
+		# Check url next release for changes.	
+			wget -O $SourceDir/_Plasma_Build/mozilla-kde-$VERSION.patch 'http://www.rosenauer.org/hg/mozilla/raw-file/default/mozilla-kde.patch'
+			wget -O $SourceDir/_Plasma_Build/firefox-kde-$VERSION.patch 'http://www.rosenauer.org/hg/mozilla/raw-file/default/firefox-kde.patch'
+			sed -i 's/Firefox/Waterfox/g' $SourceDir/_Plasma_Build/mozilla-kde-$VERSION.patch
+			sed -i 's/KMOZILLAHELPER/KWATERFOXHELPER/g' $SourceDir/_Plasma_Build/mozilla-kde-$VERSION.patch
+			sed -i 's|/usr/lib/mozilla/kmozillahelper|/opt/waterfox/kwaterfoxhelper|g' $SourceDir/_Plasma_Build/mozilla-kde-$VERSION.patch
+			sed -i 's/kmozillahelper/kwaterfoxhelper/g' $SourceDir/_Plasma_Build/mozilla-kde-$VERSION.patch
+			sed -i 's/firefox/waterfox/g' $SourceDir/_Plasma_Build/firefox-kde-$VERSION.patch
+	fi
+
+	# Apply patches if exists
+    if [  -f "$SourceDir/_Plasma_Build/mozilla-kde-$VERSION.patch" ] && [  -f "$SourceDir/_Plasma_Build/firefox-kde-$VERSION.patch" ] && [ ! -f "$SourceDir/KDE_lock" ]; then
+        cd $SourceDir
+        patch -Np1 -i $SourceDir/_Plasma_Build/mozilla-kde-$VERSION.patch
+        patch -Np1 -i $SourceDir/_Plasma_Build/firefox-kde-$VERSION.patch
+        patch -Np1 -i $SourceDir/_Plasma_Build/fix_kde_jar.mn.patch
+        patch -Np1 -i $SourceDir/_Plasma_Build/fix_browser_kde.xul.patch
+        patch -Np1 -i $SourceDir/_Plasma_Build/pgo_fix_missing_kdejs.patch
+        patch -Np1 -i $SourceDir/_Plasma_Build/fix-wifi-scanner.diff
+				echo >> "$SourceDir/KDE_lock"
+    else
+        echo "Unable to find KDE patches or patches has already been applied!"
+    fi; 
+}
+
+testConnection
+
+
+# Apply KDE Plasma patches
+echo "Applying KDE patches"
+ApplyKDE
+
+# Build Waterfox KDE Plasma Edition
+echo "Do you wish to build Waterfox now?"
+select yn in "Yes" "No" "Quit"; do
+    case $yn in
+        Yes ) 
+        rm -rvf $SourceDir/objdir; 
+        ./mach build 
+        break;;
+		      No ) break;;
+		      "Quit" )
+			exit 0
+		break;;
+		  esac
+	      done
+   
+
+echo "Do you wish to package Waterfox now?"
+select yn in "Yes" "No" "Quit"; do
+    case $yn in
+        Yes )
+	# Include kde.js
+	mkdir -p $SourceDir/objdir/dist/waterfox/browser/defaults/preferences/
+	cp -R $SourceDir/_Plasma_Build/kde.js $SourceDir/objdir/dist/waterfox/browser/defaults/preferences/
+	./mach package
+	break;;
+        No ) break;;
+        "Quit" )
+			exit 0
+		break;;
+            esac
+done
