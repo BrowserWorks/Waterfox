a01eb385e40737145360aebef2a90da952985e5d
JerryShih <hshih@mozilla.com>
2017-08-07 18:15:23 +0800

    Bug 1357299 - P6: Turn on DXVA with LAYERS_WR and ANGLE backend. v2. r=mattwoodrow
    MozReview-Commit-ID: EOOp0Dzenub

--- dom/media/platforms/wmf/WMFVideoMFTManager.cpp
+++ dom/media/platforms/wmf/WMFVideoMFTManager.cpp
@@ -485,7 +485,10 @@ WMFVideoMFTManager::InitializeDXVA()
   }
   MOZ_ASSERT(!mDXVA2Manager);
   LayersBackend backend = GetCompositorBackendType(mKnowsCompositor);
-  if (backend != LayersBackend::LAYERS_D3D11) {
+  bool useANGLE =
+      mKnowsCompositor ? mKnowsCompositor->GetCompositorUseANGLE() : false;
+  bool wrWithANGLE = (backend == LayersBackend::LAYERS_WR) && useANGLE;
+  if (backend != LayersBackend::LAYERS_D3D11 && !wrWithANGLE) {
     mDXVAFailureReason.AssignLiteral("Unsupported layers backend");
     return false;
   }
@@ -989,7 +992,7 @@ WMFVideoMFTManager::CreateD3DVideoFrame(IMFSample* aSample,
   return S_OK;
 }
 
-// Blocks until decoded sample is produced by the deoder.
+// Blocks until decoded sample is produced by the decoder.
 HRESULT
 WMFVideoMFTManager::Output(int64_t aStreamOffset,
                            RefPtr<MediaData>& aOutData)
