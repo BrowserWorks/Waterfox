9a27b6a7d95e33936456efca42e302ea59c9ddcb
Jon Coppeard <jcoppeard@mozilla.com>
2017-08-22 10:34:14 +0100

    Bug Bug 1388728 - Update module loader error handling to match the spec r=bkelly

--- dom/script/ScriptLoader.h
+++ dom/script/ScriptLoader.h
@@ -493,7 +493,9 @@ public:
 
   nsresult CreateModuleScript(ModuleLoadRequest* aRequest);
   nsresult ProcessFetchedModuleSource(ModuleLoadRequest* aRequest);
+  void CheckModuleDependenciesLoaded(ModuleLoadRequest* aRequest);
   void ProcessLoadedModuleTree(ModuleLoadRequest* aRequest);
+  bool InstantiateModuleTree(ModuleLoadRequest* aRequest);
   void StartFetchingModuleDependencies(ModuleLoadRequest* aRequest);
 
   RefPtr<mozilla::GenericPromise>
