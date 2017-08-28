40eaa609dc42ddac1ed29c3f86fae879c92729cc
Jessica Jong <jjong@mozilla.com>
2017-08-27 23:25:00 -0400

    Bug 1334051 - Part 2: Invoke attributeChangedCallback only if attribute name is in the observed attribute list. f=jdai, r=smaug
    We call attributeChangedCallback in two cases:
    1. When any of the attributes in the observed attribute list has changed, appended, removed, or replaced.
    2. When upgrading an element, for each attribute in element's attribute list that is in the observed attribute list.
    
    MozReview-Commit-ID: LKUY5ibp9RI
    * * *
    Bug 1334051 - Part 3: Optimize attributeChanged callbacks. r=smaug

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -2981,6 +2981,11 @@ public:
   static void SetupCustomElement(Element* aElement,
                                  const nsAString* aTypeExtension = nullptr);
 
+  static mozilla::dom::CustomElementDefinition*
+  GetElementDefinitionIfObservingAttr(Element* aCustomElement,
+                                      nsIAtom* aExtensionType,
+                                      nsIAtom* aAttrName);
+
   static void EnqueueLifecycleCallback(nsIDocument* aDoc,
                                        nsIDocument::ElementCallbackType aType,
                                        Element* aCustomElement,
