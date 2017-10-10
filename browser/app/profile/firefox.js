2f745c3da32db4ec3acee403a5ff0a44ddaacfb2
Alex Kontos <alex@waterfoxproject.org>
2017-10-10 20:22:06 +0100

    Revert "Remove system add-on update URL, to prevent unremovable extensions from being installed."
    This reverts commit 091abc8b28e5913ac5e80238a2574cc43fe94d81.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -61,7 +61,7 @@ pref("extensions.hotfix.certs.1.sha1Fingerprint", "91:53:98:0C:C1:86:DF:47:8F:35
 pref("extensions.hotfix.certs.2.sha1Fingerprint", "39:E7:2B:7A:5B:CF:37:78:F9:5D:4A:E0:53:2D:2F:3D:68:53:C5:60");
 
 // Check AUS for system add-on updates.
-sticky_pref("extensions.systemAddon.update.url", "");
+pref("extensions.systemAddon.update.url", "https://aus5.mozilla.org/update/3/SystemAddons/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml");
 
 // Disable add-ons that are not installed by the user in all scopes by default.
 // See the SCOPE constants in AddonManager.jsm for values to use here.
