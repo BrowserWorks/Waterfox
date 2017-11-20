15abaeb6038ead647bb33ceb64bd16bf793a0e5e
Alex Kontos <alex@waterfoxproject.org>
2017-11-20 00:05:06 +0000

    Bug 1339259 - Hold Windows AudioSession object while recycling registration. r=jimm, a=ritu
    When we UnregisterAudioSessionNotification in response to an OnSessionDisconnected message, we unintentionally decrement the refcount before restoring it. This KungFu grips us for the duration of that operation.

--- widget/windows/AudioSession.cpp
+++ widget/windows/AudioSession.cpp
@@ -282,6 +282,7 @@ AudioSession::Start()
     return NS_ERROR_FAILURE;
   }
 
+  // Increments refcount of 'this'.
   hr = mAudioSessionControl->RegisterAudioSessionNotification(this);
   if (FAILED(hr)) {
     StopInternal();
@@ -298,6 +299,7 @@ AudioSession::StopInternal()
 {
   if (mAudioSessionControl &&
       (mState == STARTED || mState == STOPPED)) {
+    // Decrement refcount of 'this'
     mAudioSessionControl->UnregisterAudioSessionNotification(this);
   }
   mAudioSessionControl = nullptr;
@@ -421,6 +423,10 @@ AudioSession::OnSessionDisconnectedInternal()
   if (!mAudioSessionControl)
     return NS_OK;
 
+  // When successful, UnregisterAudioSessionNotification will decrement the
+  // refcount of 'this'.  Start will re-increment it.  In the interim,
+  // we'll need to reference ourselves.
+  RefPtr<AudioSession> kungFuDeathGrip(this);
   mAudioSessionControl->UnregisterAudioSessionNotification(this);
   mAudioSessionControl = nullptr;
 
