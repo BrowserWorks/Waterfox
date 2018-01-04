71cb72f5dc9f6787a1039a30975723688aae18d3
Nick Alexander <nalexander@mozilla.com>
2018-01-04 15:45:56 -0800

    Bug 1419581 - Part 6: Add Android build configuration --without-google-play-services. r=dustin
    MozReview-Commit-ID: 526BCiQwlm6
    
    --HG--
    rename : mobile/android/config/mozconfigs/android-api-16/nightly-artifact => mobile/android/config/mozconfigs/android-api-16/nightly-without-google-play-services
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_without_google_play_services.py
    extra : rebase_source : b33d56c9c80a39129de525661dec459b7a40f09b

--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -369,6 +369,7 @@ class BuildOptionParser(object):
         'api-16-debug-artifact': 'builds/releng_sub_%s_configs/%s_api_16_debug_artifact.py',
         'api-16-gradle': 'builds/releng_sub_%s_configs/%s_api_16_gradle.py',
         'api-16-gradle-artifact': 'builds/releng_sub_%s_configs/%s_api_16_gradle_artifact.py',
+        'api-16-without-google-play-services': 'builds/releng_sub_%s_configs/%s_api_16_without_google_play_services.py',
         'x86': 'builds/releng_sub_%s_configs/%s_x86.py',
         'x86-old-id': 'builds/releng_sub_%s_configs/%s_x86_old_id.py',
         'x86-artifact': 'builds/releng_sub_%s_configs/%s_x86_artifact.py',
