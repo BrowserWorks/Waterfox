c32854bd513acfd3c9888fec4374bf220972b558
Andre Bargull <andre.bargull@gmail.com>
2018-01-19 05:19:40 -0800

    Bug 1422415 - Part 1: Remove HAVE_SETLOCALE because setlocale() is available on all supported platforms. r=glandium

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -7986,9 +7986,7 @@ main(int argc, char** argv, char** envp)
 
     int result;
 
-#ifdef HAVE_SETLOCALE
     setlocale(LC_ALL, "");
-#endif
 
     // Special-case stdout and stderr. We bump their refcounts to prevent them
     // from getting closed and then having some printf fail somewhere.
