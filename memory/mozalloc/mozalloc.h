b2eb83da3fd182eb734e4abb3d883af4bf69c859
Nathan Froyd <froydnj@mozilla.com>
2018-03-02 07:06:08 -0500

    Bug 1442325 - remove some dead preprocessor logic; r=glandium
    We always compile with C++11 (er, C++14), so there's no need to include
    clauses that suggest otherwise.

--- memory/mozalloc/mozalloc.h
+++ memory/mozalloc/mozalloc.h
@@ -185,15 +185,12 @@ MFBT_API void* moz_xvalloc(size_t size)
 #define MOZALLOC_THROW_IF_HAS_EXCEPTIONS throw()
 #endif
 #define MOZALLOC_THROW_BAD_ALLOC_IF_HAS_EXCEPTIONS
-#elif __cplusplus >= 201103
+#else
 /*
  * C++11 has deprecated exception-specifications in favour of |noexcept|.
  */
 #define MOZALLOC_THROW_IF_HAS_EXCEPTIONS noexcept(true)
 #define MOZALLOC_THROW_BAD_ALLOC_IF_HAS_EXCEPTIONS noexcept(false)
-#else
-#define MOZALLOC_THROW_IF_HAS_EXCEPTIONS throw()
-#define MOZALLOC_THROW_BAD_ALLOC_IF_HAS_EXCEPTIONS throw(std::bad_alloc)
 #endif
 
 #define MOZALLOC_THROW_BAD_ALLOC MOZALLOC_THROW_BAD_ALLOC_IF_HAS_EXCEPTIONS
