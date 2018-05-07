e736553a631ecd8df431ba7d44dae612d078f4bb
Brian Birtles <birtles@gmail.com>
2018-05-07 12:32:38 +0900

    Bug 1456394 - Add AnimationEffect.getTiming(); r=bz,hiro
    https://drafts.csswg.org/web-animations-1/#dom-animationeffect-gettiming

--- dom/animation/AnimationEffect.h
+++ dom/animation/AnimationEffect.h
@@ -56,6 +56,8 @@ public:
     return !SpecifiedTiming().ActiveDuration();
   }
 
+  void GetTiming(EffectTiming& aRetVal) const;
+
   already_AddRefed<AnimationEffectTimingReadOnly> Timing();
   const TimingParams& SpecifiedTiming() const
   {
