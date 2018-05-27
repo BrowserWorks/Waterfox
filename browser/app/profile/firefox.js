f96212307ef8c63793ffcb283af12da489e2e536
Peacock365 <37863161+Peacock365@users.noreply.github.com>
2018-05-27 13:43:05 +0200

    Update firefox.js
    Stops the address trimming in the URL bar, improves the UI, enhances the information given in the URL bar, shows full URL by default (stops unnecessary URL shortening).

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -342,7 +342,7 @@ pref("browser.urlbar.maxCharsForSearchSuggestions", 20);
 pref("browser.urlbar.suggest.history.onlyTyped",    false);
 
 pref("browser.urlbar.formatting.enabled", true);
-pref("browser.urlbar.trimURLs", true);
+sticky_pref("browser.urlbar.trimURLs", false);
 
 pref("browser.urlbar.oneOffSearches", true);
 
