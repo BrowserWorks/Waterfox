13e9f4ec64d9ba5df291a5a1b286e9a22c542c83
Mats Palmgren <mats@mozilla.com>
2018-06-30 01:08:54 +0200

    Bug 1470260 part 1 - Ensure that 'this' stays alive for the duration of the TickRefreshDriver call.  r=emilio

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -540,6 +540,9 @@ private:
 
     bool NotifyVsync(TimeStamp aVsyncTimestamp) override
     {
+      // IMPORTANT: All paths through this method MUST hold a strong ref on
+      // |this| for the duration of the TickRefreshDriver callback.
+
       if (!NS_IsMainThread()) {
         MOZ_ASSERT(XRE_IsParentProcess());
         // Compress vsync notifications such that only 1 may run at a time
@@ -574,6 +577,7 @@ private:
           return true;
         }
 
+        RefPtr<RefreshDriverVsyncObserver> kungFuDeathGrip(this);
         TickRefreshDriver(aVsyncTimestamp);
       }
 
