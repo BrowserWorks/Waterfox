d15d98cb38a24f1988c7e8a93a52c27a06b7068c
Alex Kontos <alex@waterfoxproject.org>
2018-08-31 15:25:41 +0100

    Bug 1469309 - Remove an unused sensor type.

--- hal/Hal.cpp
+++ hal/Hal.cpp
@@ -590,6 +590,7 @@ UnregisterSensorObserver(SensorType aSensor, ISensorObserver *aObserver) {
   AssertMainThread();
 
   if (!gSensorObservers) {
+    HAL_ERR("Un-registering a sensor when none have been registered");
     return;
   }
 
