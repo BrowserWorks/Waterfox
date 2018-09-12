936ea49d3544d8d648e925d7cfb88981a3da8f85
reikred <reikred@gmail.com>
2018-09-11 21:56:33 -0700

    Update PrintingParent.cpp
    https://bugzilla.mozilla.org/show_bug.cgi?id=1420171
    https://hg.mozilla.org/mozilla-central/rev/85b0368a8c80

--- toolkit/components/printingui/ipc/PrintingParent.cpp
+++ toolkit/components/printingui/ipc/PrintingParent.cpp
@@ -142,10 +142,12 @@ PrintingParent::ShowPrintDialog(PBrowserParent* aParent,
   // Requesting the default printer name on Linux has been removed in the child,
   // because it was causing a sandbox violation (see Bug 1329216).
   // If no printer name is set at this point, use the print settings service
-  // to get the default printer name.
-  if (printerName.IsEmpty()) {
-    mPrintSettingsSvc->GetDefaultPrinterName(getter_Copies(printerName));
-    settings->SetPrinterName(printerName);
+  // to get the default printer name, unless we're printing to file.
+  bool printToFile = false;
+  MOZ_ALWAYS_SUCCEEDS(settings->GetPrintToFile(&printToFile));
+  if (!printToFile && printerName.IsEmpty()) {
+      mPrintSettingsSvc->GetDefaultPrinterName(getter_Copies(printerName));
+      settings->SetPrinterName(printerName);
   }
   mPrintSettingsSvc->InitPrintSettingsFromPrinter(printerName, settings);
 #endif
