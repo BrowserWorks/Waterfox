e1f77c74dfad917907033d5f79befb71f8d8c167
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2018-10-09 15:15:37 -0400

    Bug 1488803 - Don't allocate PendingAction twice. r=masayuki, a=RyanVM

--- widget/windows/TSFTextStore.cpp
+++ widget/windows/TSFTextStore.cpp
@@ -4650,14 +4650,14 @@ TSFTextStore::InsertTextAtSelectionInternal(const nsAString& aInsertStr,
   TS_SELECTION_ACP oldSelection = contentForTSF.Selection().ACP();
   if (!mComposition.IsComposing()) {
     // Use a temporary composition to contain the text
-    PendingAction* compositionStart = mPendingActions.AppendElement();
+    PendingAction* compositionStart = mPendingActions.AppendElements(2);
+    PendingAction* compositionEnd = compositionStart + 1;
     compositionStart->mType = PendingAction::COMPOSITION_START;
     compositionStart->mSelectionStart = oldSelection.acpStart;
     compositionStart->mSelectionLength =
       oldSelection.acpEnd - oldSelection.acpStart;
     compositionStart->mAdjustSelection = false;
 
-    PendingAction* compositionEnd = mPendingActions.AppendElement();
     compositionEnd->mType = PendingAction::COMPOSITION_END;
     compositionEnd->mData = aInsertStr;
 
@@ -4858,10 +4858,12 @@ TSFTextStore::RecordCompositionEndAction()
       }
       // When only setting selection is necessary, we should append it.
       if (pendingAction.mAdjustSelection) {
+        LONG selectionStart = pendingAction.mSelectionStart;
+        LONG selectionLength = pendingAction.mSelectionLength;
         PendingAction* setSelection = mPendingActions.AppendElement();
         setSelection->mType = PendingAction::SET_SELECTION;
-        setSelection->mSelectionStart = pendingAction.mSelectionStart;
-        setSelection->mSelectionLength = pendingAction.mSelectionLength;
+        setSelection->mSelectionStart = selectionStart;
+        setSelection->mSelectionLength = selectionLength;
         setSelection->mSelectionReversed = false;
       }
       // Remove the redundant pending composition.
