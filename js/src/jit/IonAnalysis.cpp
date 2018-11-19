979aef58f6b16007ea6145c27045940abc54ba3d
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2018-11-19 18:28:43 -0200

    Bug 1502013 - RemoveUnmarkedBlocks should only mark operands of removed blocks. r=jandem, a=lizzard

--- js/src/jit/IonAnalysis.cpp
+++ js/src/jit/IonAnalysis.cpp
@@ -2340,7 +2340,7 @@ jit::RemoveUnmarkedBlocks(MIRGenerator* mir, MIRGraph& graph, uint32_t numMarked
         // bailout.
         for (PostorderIterator it(graph.poBegin()); it != graph.poEnd();) {
             MBasicBlock* block = *it++;
-            if (!block->isMarked())
+            if (block->isMarked())
                 continue;
 
             FlagAllOperandsAsHavingRemovedUses(mir, block);
