c46a785f0ebde4a3e912f487244ec2ef6f7a4d3c
Alex Kontos <alex@waterfoxproject.org>
2019-03-07 15:14:12 +0000

    Disable Telemetry and Health Report in conf.sh.

--- browser/branding/unofficial/configure.sh
+++ browser/branding/unofficial/configure.sh
@@ -2,4 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Nightly
+MOZ_APP_DISPLAYNAME=Waterfox
+MOZ_TELEMETRY_REPORTING=
+MOZ_TELEMETRY_ON_BY_DEFAULT=
+MOZ_SERVICES_HEALTHREPORT=
+MOZ_DATA_REPORTING=
