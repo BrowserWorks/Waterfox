732b10f0c8b97ce0d57ceaa93188c9993db0f651
Alex Kontos <alex@waterfoxproject.org>
2019-05-02 19:35:32 +0100

    Use a simpler update URL for alpha.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -154,7 +154,7 @@ pref("app.update.staging.enabled", true);
 
 // Update service URL:
 // OLD: https://www.waterfox.net/update/%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%SYSTEM_CAPABILITIES%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml
-pref("app.update.url", "https://www.waterfox.net/update/%PRODUCT%/%VERSION%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml");
+pref("app.update.url", "https://www.waterfox.net/update/%PRODUCT%/%VERSION%/%CHANNEL%/%OS%/update.xml");
 // app.update.url.manual is in branding section
 // app.update.url.details is in branding section
 
