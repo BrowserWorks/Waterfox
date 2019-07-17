806d84c1f1fe3e204ae5e08f8b4ce55101a4da0e
Victor Porof <vporof@mozilla.com>
2019-07-17 09:35:07 +0200

    Bug 1558517 - Pre 2: Move the **/*.*html overrides to recommended.js, r=standard8
    Differential Revision: https://phabricator.services.mozilla.com/D34534
    
    --HG--
    extra : source : cc5adb1ac2edeb5dfc06520b1391c00d87fd7bf9
    extra : intermediate-source : 570041e70b591ef492ce94ea15d4021fae1498a6

--- tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js
@@ -50,6 +50,13 @@ module.exports = {
   },
 
   "overrides": [{
+    // eslint-plugin-html handles eol-last slightly different - it applies to
+    // each set of script tags, so we turn it off here.
+    "files": "**/*.*html",
+    "rules": {
+      "eol-last": "off",
+    },
+  }, {
     // Turn off use-services for xml files. XBL bindings are going away, and
     // working out the valid globals for those is difficult.
     "files": "**/*.xml",
@@ -81,6 +88,7 @@ module.exports = {
 
   // When adding items to this file please check for effects on sub-directories.
   "plugins": [
+    "html",
     "no-unsanitized",
     "prettier",
   ],
@@ -383,5 +391,12 @@ module.exports = {
 
     // Requires or disallows a whitespace (space or tab) beginning a comment
     "spaced-comment": ["error", "always", { "markers": ["#"] }],
+  },
+
+  // The html plugin is enabled via a command line option on eslint. To avoid
+  // bad interactions with the xml preprocessor in eslint-plugin-mozilla, we
+  // turn off processing of the html plugin for .xml files.
+  "settings": {
+    "html/xml-extensions": [ ".xhtml" ]
   },
 };
