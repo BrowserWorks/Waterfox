c060cda85f78799ab922410c208eab1955d57cbe
ffxbld <ffxbld@mozilla.com>
2019-08-15 13:04:53 +0000

    No Bug, mozilla-esr68 repo-update HSTS HPKP blocklist remote-settings tld-suffixes - a=repo-update r=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D42126
    
    --HG--
    extra : rebase_source : 2720c44f7c8b9894bbe85bfc320dbc22b071eff2

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -1155,4 +1155,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1573153490848000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1574341214832000);
