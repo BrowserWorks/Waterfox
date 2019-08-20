8d7cf888a1b597507477c13bc33535fe3dc7d813
Andrew McCreight <continuation@gmail.com>
2019-08-20 20:03:18 +0000

    Bug 1574972 - Make nsJSUtils::GetCallingLocation() fallible. r=smaug, a=RyanVM
    Some German web sites seem to have started to have file names with
    hundreds of thousands of bytes long, which is causing OOM crashes on
    Android.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42674
    
    --HG--
    extra : source : 488e2bd20e1b5581fd96650b3115b92d093bdc6d
    extra : intermediate-source : 0859d278bfd2cb77029c61d464be746a5f34ffc5

--- dom/base/nsJSUtils.cpp
+++ dom/base/nsJSUtils.cpp
@@ -46,8 +46,7 @@ bool nsJSUtils::GetCallingLocation(JSContext* aContext, nsACString& aFilename,
     return false;
   }
 
-  aFilename.Assign(filename.get());
-  return true;
+  return aFilename.Assign(filename.get(), fallible);
 }
 
 bool nsJSUtils::GetCallingLocation(JSContext* aContext, nsAString& aFilename,
@@ -57,8 +56,7 @@ bool nsJSUtils::GetCallingLocation(JSContext* aContext, nsAString& aFilename,
     return false;
   }
 
-  aFilename.Assign(NS_ConvertUTF8toUTF16(filename.get()));
-  return true;
+  return aFilename.Assign(NS_ConvertUTF8toUTF16(filename.get()), fallible);
 }
 
 uint64_t nsJSUtils::GetCurrentlyRunningCodeInnerWindowID(JSContext* aContext) {
