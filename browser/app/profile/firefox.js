7a54c86a04da2549e7485dfa2c11d6e0eed3e89f
Adam Gashlin <agashlin@mozilla.com>
2019-08-26 23:38:50 +0000

    Bug 1572844 - Consistently check for MOZ_BITS_DOWNLOAD. r=bytesized a=jcristau
    BITS download can be disabled on Windows. This patch checks for the
    config/define MOZ_BITS_DOWNLOAD when including the C++ parts of the
    service, exposing the service in JS, setting the pref, and including
    BITS-specific tests. For consistency and simplicity it also removes
    the Windows checks; the configure system won't allow
    MOZ_BITS_DOWNLOAD if not on Windows.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43493
    
    --HG--
    extra : rebase_source : fd259817325949c15442d47e7aaa5c0556c1e1b7
    extra : source : 7ce6426b7653e89258f3bfd74903e9b97390399c

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -177,7 +177,7 @@ pref("app.update.idletime", 60);
 pref("app.update.service.enabled", true);
 #endif
 
-#ifdef XP_WIN
+#ifdef MOZ_BITS_DOWNLOAD
 // If set to true, the Update Service will attempt to use Windows BITS to
 // download updates and will fallback to downloading internally if that fails.
 pref("app.update.BITS.enabled", true);
