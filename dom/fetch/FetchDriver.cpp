eea50168aa750efa24a2d668f8e2deff68b1cf04
Alex Kontos <alex@waterfoxproject.org>
2019-09-01 21:16:33 +0100

    Bug 1571283 - Add null check for mObserver r=baku a=RyanVM

--- dom/fetch/FetchDriver.cpp
+++ dom/fetch/FetchDriver.cpp
@@ -484,7 +484,12 @@ FetchDriver::OnStartRequest(nsIRequest* aRequest,
 
   // We should only get to the following code once.
   MOZ_ASSERT(!mPipeOutputStream);
-  MOZ_ASSERT(mObserver);
+
+  if (!mObserver) {
+    MOZ_ASSERT(false, "We should have mObserver here.");
+    FailWithNetworkError();
+    return NS_ERROR_UNEXPECTED;
+  }
 
   RefPtr<InternalResponse> response;
   nsCOMPtr<nsIChannel> channel = do_QueryInterface(aRequest);
@@ -980,6 +985,7 @@ FetchDriver::SetRequestHeaders(nsIHttpChannel* aChannel) const
 void
 FetchDriver::Aborted()
 {
+  MOZ_DIAGNOSTIC_ASSERT(NS_IsMainThread());
   if (mObserver) {
   #ifdef DEBUG
     mResponseAvailableCalled = true;
