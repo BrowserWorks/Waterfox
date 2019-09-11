7533117e578cd07828abb9b394d38ea4f996b35f
Tom Ritter <tom@mozilla.com>
2019-09-11 00:44:41 -0500

    Bug 1547519 - Rename NS_STRINGIFY to MOZ_STRINGIFY, move to mfbt, and unify stragglers. r=glandium, a=RyanVM

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -256,8 +256,8 @@ char** gArgv;
 
 #include "buildid.h"
 
-static const char gToolkitVersion[] = NS_STRINGIFY(GRE_MILESTONE);
-static const char gToolkitBuildID[] = NS_STRINGIFY(MOZ_BUILDID);
+static const char gToolkitVersion[] = MOZ_STRINGIFY(GRE_MILESTONE);
+static const char gToolkitBuildID[] = MOZ_STRINGIFY(MOZ_BUILDID);
 
 static nsIProfileLock* gProfileLock;
 
@@ -851,7 +851,7 @@ nsXULAppInfo::GetIsOfficialBranding(bool* aResult) {
 
 NS_IMETHODIMP
 nsXULAppInfo::GetDefaultUpdateChannel(nsACString& aResult) {
-  aResult.AssignLiteral(NS_STRINGIFY(MOZ_UPDATE_CHANNEL));
+  aResult.AssignLiteral(MOZ_STRINGIFY(MOZ_UPDATE_CHANNEL));
   return NS_OK;
 }
 
@@ -1452,7 +1452,7 @@ static inline void DumpVersion() {
 
   // Use the displayed version
   // For example, for beta, we would display 42.0b2 instead of 42.0
-  printf("%s", NS_STRINGIFY(MOZ_APP_VERSION_DISPLAY));
+  printf("%s", MOZ_STRINGIFY(MOZ_APP_VERSION_DISPLAY));
 
   if (gAppData->copyright) {
     printf(", %s", (const char*)gAppData->copyright);
@@ -2160,7 +2160,8 @@ static void SubmitDowngradeTelemetry(const nsCString& aLastVersion,
       w.StringProperty("buildId", gAppData->buildID);
       w.StringProperty("name", gAppData->name);
       w.StringProperty("version", gAppData->version);
-      w.StringProperty("displayVersion", NS_STRINGIFY(MOZ_APP_VERSION_DISPLAY));
+      w.StringProperty("displayVersion",
+                       MOZ_STRINGIFY(MOZ_APP_VERSION_DISPLAY));
       w.StringProperty("vendor", gAppData->vendor);
       w.StringProperty("platformVersion", gToolkitVersion);
 #  ifdef TARGET_XPCOM_ABI
@@ -2291,7 +2292,7 @@ static ReturnAbortOnError CheckDowngrade(nsIFile* aProfileDir,
     nsCString profileName;
     profileName.AssignLiteral("default");
 #  ifdef MOZ_DEDICATED_PROFILES
-    profileName.Append("-" NS_STRINGIFY(MOZ_UPDATE_CHANNEL));
+    profileName.Append("-" MOZ_STRINGIFY(MOZ_UPDATE_CHANNEL));
 #  endif
     nsCOMPtr<nsIToolkitProfile> newProfile;
     rv = aProfileSvc->CreateUniqueProfile(nullptr, profileName,
@@ -3220,7 +3221,7 @@ int XREMain::XRE_mainInit(bool* aExitFlag) {
       CrashReporter::AnnotateCrashReport(CrashReporter::Annotation::BuildID,
                                          nsDependentCString(mAppData->buildID));
 
-    nsDependentCString releaseChannel(NS_STRINGIFY(MOZ_UPDATE_CHANNEL));
+    nsDependentCString releaseChannel(MOZ_STRINGIFY(MOZ_UPDATE_CHANNEL));
     CrashReporter::AnnotateCrashReport(
         CrashReporter::Annotation::ReleaseChannel, releaseChannel);
 #ifdef MOZ_LINKER
@@ -3628,7 +3629,7 @@ static void SetShutdownChecks() {
   gShutdownChecks = SCM_CRASH;
 #  endif  // MOZ_CODE_COVERAGE
 #else
-  const char* releaseChannel = NS_STRINGIFY(MOZ_UPDATE_CHANNEL);
+  const char* releaseChannel = MOZ_STRINGIFY(MOZ_UPDATE_CHANNEL);
   if (strcmp(releaseChannel, "nightly") == 0 ||
       strcmp(releaseChannel, "default") == 0) {
     gShutdownChecks = SCM_RECORD;
@@ -3727,7 +3728,7 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
   mozilla::Telemetry::InitIOReporting(gAppData->xreDirectory);
 #  else
   {
-    const char* releaseChannel = NS_STRINGIFY(MOZ_UPDATE_CHANNEL);
+    const char* releaseChannel = MOZ_STRINGIFY(MOZ_UPDATE_CHANNEL);
     if (strcmp(releaseChannel, "nightly") == 0 ||
         strcmp(releaseChannel, "default") == 0) {
       mozilla::Telemetry::InitIOReporting(gAppData->xreDirectory);
