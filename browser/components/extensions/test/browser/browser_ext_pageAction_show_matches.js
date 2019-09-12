59e527f05977389c77ced7e21182b0ac14b9767d
Shane Caraveo <scaraveo@mozilla.com>
2019-09-12 18:22:49 +0000

    Bug 1537982 - Fix intermittent in browser_ext_pageAction_show_matches.js. r=rpl, a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D45442
    
    --HG--
    extra : source : 615f56b6ae6eca410e1bf2473abccc9eb795f9c9

--- browser/components/extensions/test/browser/browser_ext_pageAction_show_matches.js
+++ browser/components/extensions/test/browser/browser_ext_pageAction_show_matches.js
@@ -32,7 +32,7 @@ let tests = [
     page_action: {
       show_matches: ["<all_urls>"],
     },
-    shown: [true, true, false, false],
+    shown: [true, true, false],
   },
   {
     name: "Test hide_matches overrides all_urls.",
@@ -40,25 +40,21 @@ let tests = [
       show_matches: ["<all_urls>"],
       hide_matches: ["*://mochi.test/*"],
     },
-    shown: [true, false, false, false],
+    shown: [true, false, false],
   },
   {
     name: "Test shown only for show_matches.",
     page_action: {
       show_matches: ["*://mochi.test/*"],
     },
-    shown: [false, true, false, false],
+    shown: [false, true, false],
   },
 ];
 
 // For some reason about:rights and about:about used to behave differently (maybe
-// because only the latter is privileged?) so both should be tested.
-let urls = [
-  "http://example.com/",
-  "http://mochi.test:8888/",
-  "about:rights",
-  "about:about",
-];
+// because only the latter is privileged?) so both should be tested.  about:about
+// is used in the test as the base tab.
+let urls = ["http://example.com/", "http://mochi.test:8888/", "about:rights"];
 
 function getId(tab) {
   let {
@@ -101,7 +97,7 @@ add_task(async function test_pageAction_default_show_tabs() {
   );
   let switchTab = await BrowserTestUtils.openNewForegroundTab(
     gBrowser,
-    "about:blank",
+    "about:about",
     true,
     true
   );
@@ -126,6 +122,7 @@ add_task(async function test_pageAction_default_show_tabs() {
 
       info("Check switched tab.");
       await BrowserTestUtils.switchTab(gBrowser, switchTab);
+      await check(extension, switchTab, false, msg + " (about:about)");
       await BrowserTestUtils.switchTab(gBrowser, tab);
       await check(extension, tab, expected, msg + " (switched)");
 
