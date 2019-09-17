38129b141bddcb58074a07ed4f3af0c1a2ab9e39
Dana Keeler <dkeeler@mozilla.com>
2019-09-17 23:55:29 +0000

    bug 1579906 - Disable sending new TLS error reports while we rethink our approach. r=kjacobs, a=RyanVM
    This also disables the UI on TLS error pages.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46222
    
    --HG--
    extra : source : a80b7ebbd675fa5874d6cb0957757a47be9396a2
    extra : amend_source : 377ead46b4dc21b8b3df01f9e18381d122aaf61d

--- security/manager/ssl/security-prefs.js
+++ security/manager/ssl/security-prefs.js
@@ -140,7 +140,7 @@ pref("security.webauth.webauthn_enable_usbtoken", false);
 pref("security.webauth.webauthn_enable_usbtoken", true);
 #endif
 
-pref("security.ssl.errorReporting.enabled", true);
+pref("security.ssl.errorReporting.enabled", false);
 pref("security.ssl.errorReporting.url", "https://incoming.telemetry.mozilla.org/submit/sslreports/");
 pref("security.ssl.errorReporting.automatic", false);
 
