175d9b3155833c37d3a03678d640b7a5b41ea9f4
Peacock365 <37863161+Peacock365@users.noreply.github.com>
2019-09-18 16:03:54 +0200

    (WF56) Enable containers by default.
    See header

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1521,9 +1521,9 @@ pref("privacy.usercontext.about_newtab_segregation.enabled", true);
 // 0 disables long press, 1 when clicked, the menu is shown, 2 the menu is shown after X milliseconds.
 pref("privacy.userContext.longPressBehavior", 2);
 #else
-pref("privacy.userContext.enabled", false);
-pref("privacy.userContext.ui.enabled", false);
-pref("privacy.usercontext.about_newtab_segregation.enabled", false);
+pref("privacy.userContext.enabled", true);
+pref("privacy.userContext.ui.enabled", true);
+pref("privacy.usercontext.about_newtab_segregation.enabled", true);
 
 // 0 disables long press, 1 when clicked, the menu is shown, 2 the menu is shown after X milliseconds.
 pref("privacy.userContext.longPressBehavior", 0);
