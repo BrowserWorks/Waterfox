c314ba515daf04d6a444f0cb464c7260948be19c
Andrew Osmond <aosmond@mozilla.com>
2019-09-26 15:02:27 +0000

    Bug 1583238 - Check if ImageBitmap was closed in WriteStructuredClone.

--- dom/canvas/ImageBitmap.cpp
+++ dom/canvas/ImageBitmap.cpp
@@ -1409,6 +1409,11 @@ ImageBitmap::WriteStructuredClone(JSStructuredCloneWriter* aWriter,
   MOZ_ASSERT(aWriter);
   MOZ_ASSERT(aImageBitmap);
 
+  if (!aImageBitmap->mData) {
+    // A closed image cannot be cloned.
+    return false;
+  }
+
   const uint32_t picRectX = BitwiseCast<uint32_t>(aImageBitmap->mPictureRect.x);
   const uint32_t picRectY = BitwiseCast<uint32_t>(aImageBitmap->mPictureRect.y);
   const uint32_t picRectWidth = BitwiseCast<uint32_t>(aImageBitmap->mPictureRect.width);
