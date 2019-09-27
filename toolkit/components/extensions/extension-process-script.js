c13eae980acb036a2c42e97fd22df0578f454a8c
hawkeye116477 <hawkeye116477@gmail.com>
2019-09-27 11:50:24 +0200

    Bug 1356334: Part 4 - Add a helper to synchronously get an extension's name from its ID. r=billm

--- toolkit/components/extensions/extension-process-script.js
+++ toolkit/components/extensions/extension-process-script.js
@@ -303,6 +303,7 @@ ExtensionManager = {
       policy = new WebExtensionPolicy({
         id: data.id,
         mozExtensionHostname: data.uuid,
+        name: extension.name,
         baseURL: data.resourceURL,
 
         permissions: Array.from(data.permissions),
