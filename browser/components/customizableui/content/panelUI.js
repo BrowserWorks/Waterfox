0c208adf7e7d89f5ae7b9fd430b944b3bf2f7ebf
hawkeye116477 <hawkeye116477@gmail.com>
2019-10-21 20:58:19 +0200

    Change look of message about restart need after locale change.

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -473,28 +473,6 @@ const PanelUI = {
     this.multiView.showSubView("PanelUI-helpView", aAnchor);
   },
 
-  restartBrowser() {
-    let RestartMsg = Services.strings.createBundle("chrome://browser/locale/browser.properties");
-    try {
-          if (Services.prefs.getBoolPref("browser.restart.requireconfirm")) {
-            if (Services.prompt.confirm(null, RestartMsg.formatStringFromName("restartPromptTitle.label", [Services.strings.createBundle("chrome://branding/locale/brand.properties").GetStringFromName("brandShortName")], 1),
-            RestartMsg.GetStringFromName("addonInstallRestartButton"))) {
-                if (Services.prefs.getBoolPref("browser.restart.purgecache")) {
-                    Services.appinfo.invalidateCachesOnRestart();
-                }
-                Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);
-            }
-        } else {
-            if (Services.prefs.getBoolPref("browser.restart.purgecache")) {
-                Services.appinfo.invalidateCachesOnRestart();
-            }
-            Services.startup.quit(Services.startup.eRestart | Services.startup.eAttemptQuit);
-        }
-    } catch (e) {
-        throw new Error("We're sorry but something has gone wrong with 'restartBrowser' " + e);
-    }
-  },
-
   /**
    * Shows a subview in the panel with a given ID.
    *
--- browser/components/preferences/in-content/main.js
+++ browser/components/preferences/in-content/main.js
@@ -176,7 +176,7 @@ var gMainPane = {
 	  	selectedLocale.value = Services.prefs.getCharPref('general.useragent.locale');
 	  }
   },
-  
+
   updateLocale: function ()
   {
     let alertsService = Cc["@mozilla.org/alerts-service;1"].getService(Ci.nsIAlertsService);
@@ -184,13 +184,17 @@ var gMainPane = {
     let contentLocaleUpdate = selectedLocale + ",en-us,en";
     let contentLocaleUpdated = contentLocaleUpdate.toLowerCase();
 
-    if (selectedLocale === Services.prefs.getCharPref('general.useragent.locale')) return;
-	
+    if (selectedLocale === Services.prefs.getCharPref('general.useragent.locale'))
+    {
+      document.getElementById("confirmBrowserLanguage").hidden = "true";
+      return;
+    }
+
     if (selectedLocale != "") {
       Services.prefs.setCharPref('general.useragent.locale', selectedLocale);
       Services.prefs.setCharPref('intl.accept_languages', contentLocaleUpdated);
       Services.prefs.setBoolPref('intl.locale.matchOS', false);
-      alertsService.showAlertNotification("",  "Restart Waterfox", "You'll need to restart Waterfox to see your selected locale.");
+      document.getElementById("confirmBrowserLanguage").hidden = "";
     }
   },
   _updateLocale: function (e)
@@ -199,7 +203,7 @@ var gMainPane = {
 		  this.updateLocale();
     }
 },
-    
+
   enableE10SChange() {
     if ((AppConstants.E10S_TESTING_ONLY) && (!AppConstants.isPlatformAndVersionAtMost("macosx", 13))) {
       let e10sCheckbox = document.getElementById("e10sAutoStart");
