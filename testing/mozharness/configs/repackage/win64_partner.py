c4b494e1afe0925725dce9740f797ea21d7e6751
Tom Prince <mozilla@hocat.ca>
2019-11-12 15:19:55 -0700

    Bug 1595664: Remove unused tooltool manifests from repackge jobs; r=Callek a=tomprince
    Differential Revision: https://phabricator.services.mozilla.com/D52732
    
    --HG--
    extra : rebase_source : 7ada0f0b0a9cc4cf75c3d5bc82f31e717efaa844

--- testing/mozharness/configs/repackage/win64_partner.py
+++ testing/mozharness/configs/repackage/win64_partner.py
@@ -6,7 +6,6 @@ config = {
     "repack_id": os.environ.get("REPACK_ID"),
 
     # ToolTool
-    "tooltool_manifest_src": 'browser\\config\\tooltool-manifests\\{}\\releng.manifest'.format(platform),
     'tooltool_cache': os.environ.get('TOOLTOOL_CACHE'),
 
     'run_configure': False,
