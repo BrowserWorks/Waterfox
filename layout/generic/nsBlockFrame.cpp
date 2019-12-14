65de6f0211105e77cbe9c4db1ec344224690bfbf
hawkeye116477 <hawkeye116477@gmail.com>
2019-12-14 22:58:45 +0100

    Bug 488725: Don't position floats in a nowrap context until hitting a break opportunity. r=dbaron

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -3908,6 +3908,7 @@ nsBlockFrame::ReflowInlineFrames(BlockReflowInput& aState,
           aState.FloatManager()->PopState(&floatManagerState);
           // Clear out float lists
           aState.mCurrentLineFloats.DeleteAll();
+          MOZ_ASSERT(aState.mNoWrapFloats.IsEmpty());
           aState.mBelowCurrentLineFloats.DeleteAll();
         }
 
@@ -4539,6 +4540,9 @@ nsBlockFrame::PlaceLine(BlockReflowInput& aState,
                         nscoord& aAvailableSpaceBSize,
                         bool* aKeepReflowGoing)
 {
+  // Try to position the floats in a nowrap context.
+  aLineLayout.FlushNoWrapFloats();
+
   // Trim extra white-space from the line before placing the frames
   aLineLayout.TrimTrailingWhiteSpace();
 
