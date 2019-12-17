88e7bca4020d7a070b7e9baf20303c4370045df0
hawkeye116477 <hawkeye116477@gmail.com>
2019-12-17 12:58:59 +0100

    Bug 1405286: Part 1 - Allow retrieving the delivery target from retargetable requests. r=dragana
    After data delivery for a request has been retargeted, there's no reliable way to get the appropriate event target to re-dispatch data events after asynchronous processing. 
    While it's technically possible to retrieve the current thread from OnDataAvailable callbacks and re-use that for later dispatch, that approach has some issues: 
    1) It's not currently possible to reliably map the current thread to the thread pool that owns it. That means that if data delivery is being targetted to a thread pool, attempts to redispatch events to the previous delivery thread might lead to long delays when one thread in a pool is blocked. 
    2) If a filter wishes to dispatch data events to the wrapped listeners before it's recieved any data (as extensions StreamFilters sometimes do), there's no way to determine the proper event target without waiting for initial data to be received. Simply returning the correct event target from the request solves both of these problems.

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -3352,6 +3352,19 @@ HttpChannelChild::RetargetDeliveryTo(nsIEventTarget* aNewTarget)
   return NS_OK;
 }
 
+NS_IMETHODIMP
+HttpChannelChild::GetDeliveryTarget(nsIEventTarget** aEventTarget)
+{
+  MutexAutoLock lock(mEventTargetMutex);
+
+  nsCOMPtr<nsIEventTarget> target = mODATarget;
+  if (!mODATarget) {
+    target = GetCurrentThreadEventTarget();
+  }
+  target.forget(aEventTarget);
+  return NS_OK;
+}
+
 void
 HttpChannelChild::ResetInterception()
 {
