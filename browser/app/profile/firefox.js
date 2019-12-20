2ed5fa7bc3cf85aa50c6d401bf1351e8a363da0f
Robert Strong <robert.bugzilla@gmail.com>
2019-12-20 14:37:44 -0500

    Bug 1596778 - Fallback to the normal update path when a replace request fails. r=mhowell, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D54086

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -159,12 +159,7 @@ pref("app.update.silent", false);
 
 // If set to true, the Update Service will apply updates in the background
 // when it finishes downloading them.
-#ifdef XP_WIN
-// Staging is disabled on Windows due to bug 1596778
-pref("app.update.staging.enabled", false);
-#else
 pref("app.update.staging.enabled", true);
-#endif
 
 // Update service URL:
 pref("app.update.url", "https://aus5.mozilla.org/update/6/%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%SYSTEM_CAPABILITIES%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml");
