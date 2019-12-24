72e6bbbd6c4c85a0d36545410a416f66993bcb31
Junior Hsu <juhsu@mozilla.com>
2019-12-24 16:14:54 +0000

    Bug 1604851, r=dragana, a=RyanVM
    No need to optimize one lock here since telemetry tells us only 1 out of 1m request with alt-svc
    https://searchfox.org/mozilla-central/rev/b243debf6235b050b42fd2eb615fdc729636ca6b/netwerk/protocol/http/nsHttpChannel.cpp#6670
    
    Differential Revision: https://phabricator.services.mozilla.com/D57925
    
    --HG--
    extra : source : e7d3c0687268354d8fc2dd194551750110af82ab
    extra : intermediate-source : ff5864fa16d8adea6e7e029c3054154cefde9b1f

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -303,6 +303,8 @@ nsHttpHandler::nsHttpHandler()
       mNextChannelId(1),
       mLastActiveTabLoadOptimizationLock(
           "nsHttpConnectionMgr::LastActiveTabLoadOptimization"),
+      mSpdyBlacklistLock(
+          "nsHttpHandler::SpdyBlacklist"),
       mThroughCaptivePortal(false) {
   LOG(("Creating nsHttpHandler [this=%p].\n", this));
 
@@ -2704,11 +2706,17 @@ bool nsHttpHandler::IsBeforeLastActiveTabLoadOptimization(
 }
 
 void nsHttpHandler::BlacklistSpdy(const nsHttpConnectionInfo* ci) {
+  MOZ_ASSERT(OnSocketThread(), "not on socket thread");
+
   mConnMgr->BlacklistSpdy(ci);
-  mBlacklistedSpdyOrigins.PutEntry(ci->GetOrigin());
+  if (!mBlacklistedSpdyOrigins.Contains(ci->GetOrigin())) {
+    MutexAutoLock lock(mSpdyBlacklistLock);
+    mBlacklistedSpdyOrigins.PutEntry(ci->GetOrigin());
+  }
 }
 
 bool nsHttpHandler::IsSpdyBlacklisted(const nsHttpConnectionInfo* ci) {
+  MutexAutoLock lock(mSpdyBlacklistLock);
   return mBlacklistedSpdyOrigins.Contains(ci->GetOrigin());
 }
 
