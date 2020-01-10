70de8c5bb28b09ae3d8a374832bb83d72a82185e
hawkeye116477 <hawkeye116477@gmail.com>
2020-01-10 13:34:11 +0100

    Add ICU patch for glibc 2.26.

--- intl/update-icu.sh
+++ intl/update-icu.sh
@@ -82,6 +82,7 @@ for patch in \
  u_setMemoryFunctions-callconvention-anachronism-msvc.diff \
  bug-1373763-convertToPosix-stack-value-out-of-scope.diff \
  bug-1380083 \
+ glibc_2.26.patch \
 ; do
   echo "Applying local patch $patch"
   patch -d ${icu_dir}/../../ -p1 --no-backup-if-mismatch < ${icu_dir}/../icu-patches/$patch
