a80d9622a32a9d347b27e7c0a9e279a2f7eacd88
hawkeye116477 <hawkeye116477@gmail.com>
2020-01-15 21:08:05 +0100

    Bug 1564643 - Update to latest mp4parse that fixes rust 1.37 deprecation errors. r!kinetik r=kinetik

--- media/mp4parse-rust/update-rust.sh
+++ media/mp4parse-rust/update-rust.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Default version.
-VER="26e614bbcb4d7322dc2e4b7e15391bdb30b9f7be"
+VER="b10bc03bc279b2312f6a4ddfc96230577dcffe56"
 
 # Accept version or commit from the command line.
 if test -n "$1"; then
