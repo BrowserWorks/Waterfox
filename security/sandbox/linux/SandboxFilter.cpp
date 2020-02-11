4e78bb18b47da2ad26a739e00165f9a980ac48d7
Emilio Cobos Álvarez <emilio@crisal.io>
2020-02-11 18:20:34 +0000

    Bug 1614535 - Whitelist pread64 in the common policy. r=gcp
    Differential Revision: https://phabricator.services.mozilla.com/D62468
    
    --HG--
    extra : moz-landing-system : lando

--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -500,6 +500,7 @@ class SandboxPolicyCommon : public SandboxPolicyBase {
         return Allow();
 
         // Simple I/O
+      case __NR_pread64:
       case __NR_write:
       case __NR_read:
       case __NR_readv:
@@ -1020,7 +1021,6 @@ class ContentSandboxPolicy : public SandboxPolicyCommon {
 
       CASES_FOR_getdents:
       case __NR_writev:
-      case __NR_pread64:
 #ifdef DESKTOP
       case __NR_pwrite64:
       case __NR_readahead:
