7cc2eaa7d10fdd7b5bbb00dba0aac0e06018e83d
Sylvestre Ledru <sledru@mozilla.com>
2020-02-24 22:04:16 +0000

    Bug 1616780 - Add __NR_sched_getattr to the list of thread function r=jld
    Introduced in: https://gitlab.gnome.org/GNOME/glib/commit/8aeca4fa647bfd0f35c4a86b1e6ca6e955519ca5

--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -769,6 +769,7 @@ public:
 
     case __NR_getpriority:
     case __NR_setpriority:
+    case __NR_sched_getattr:
     case __NR_sched_get_priority_min:
     case __NR_sched_get_priority_max:
     case __NR_sched_getscheduler:
