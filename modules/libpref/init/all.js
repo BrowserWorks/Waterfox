984140089afaa89580a22c414a80f500f6805026
Peacock365 <37863161+Peacock365@users.noreply.github.com>
2020-02-29 11:20:28 +0100

    (Classic) Privacy: No more information about the graphics driver for debugging purposes.
    This reduces possible misuse for hardware fingerprinting. See here:
    https://developer.mozilla.org/en-US/docs/Web/API/WEBGL_debug_renderer_info
    
    Various Firefox-related privacy guides recommend disabling this setting. No website breakage is expected here.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4747,7 +4747,7 @@ pref("webgl.perf.spew-frame-allocs", true);
 
 pref("webgl.enable-webgl2", true);
 
-pref("webgl.enable-debug-renderer-info", true);
+pref("webgl.enable-debug-renderer-info", false);
 pref("webgl.renderer-string-override", "");
 pref("webgl.vendor-string-override", "");
 
