ee8e252c890a25f07077a04b1f1c0c797d8eb0fa
hawkeye116477 <hawkeye116477@gmail.com>
2020-03-01 21:31:15 +0100

    Use more appropriate version and application name for cmdline info.

--- browser/branding/unofficial/configure.sh
+++ browser/branding/unofficial/configure.sh
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Waterfox
+MOZ_APP_DISPLAYNAME="Waterfox Classic"
 MOZ_APP_REMOTINGNAME=waterfox-classic
 
 # Hard code profile locations due to rebranding changing the default location
