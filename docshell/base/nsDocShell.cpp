93a181e4ab88983dd98eb1ae3113ccbb68bd30bd
Olli Pettay <Olli.Pettay@helsinki.fi>
2020-04-02 14:43:50 +0000

    Bug 1620818 - Release nsDocShell::mContentViewer properly. r=nika,peterv a=dveditz
    Differential Revision: https://phabricator.services.mozilla.com/D69383
    
    --HG--
    extra : source : 8755eccf58cd6bd4fc1f00e6bcf45681d41da314
    extra : intermediate-source : a4d7d228e423decf11c3981f5870574daecb5457

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -886,6 +886,12 @@ nsDocShell::~nsDocShell()
     shPrivate->SetRootDocShell(nullptr);
   }
 
+  if (mContentViewer) {
+    mContentViewer->Close(nullptr);
+    mContentViewer->Destroy();
+    mContentViewer = nullptr;
+  }
+
   if (--gDocShellCount == 0) {
     NS_IF_RELEASE(sURIFixup);
   }
