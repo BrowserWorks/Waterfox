10fc94483607bb95b4a759b2260178ee3d4bd85b
hawkeye116477 <hawkeye116477@gmail.com>
2020-04-20 14:07:07 +0200

    Bug 1411754: Rename PresShell::DestroyFramesFor to DestroyFramesForAndRestyle.
    I'm drive-by removing the comment about the frame tree state because I looked into it, and the answer is: we properly restore it. 
    
    The gotcha is that we retain it too much, indeed, we retain it enough that it can leak. See bug 1397239.

--- dom/base/ShadowRoot.cpp
+++ dom/base/ShadowRoot.cpp
@@ -263,8 +263,7 @@ ShadowRoot::DistributionChanged()
     return;
   }
 
-  // FIXME(emilio): Rename this to DestroyFramesForAndRestyle?
-  shell->DestroyFramesFor(host);
+  shell->DestroyFramesForAndRestyle(host);
 }
 
 const HTMLContentElement*
@@ -511,7 +510,7 @@ ShadowRoot::AttributeChanged(nsIDocument* aDocument,
     return;
   }
 
-  shell->DestroyFramesFor(aElement);
+  shell->DestroyFramesForAndRestyle(aElement);
 }
 
 bool
