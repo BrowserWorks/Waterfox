2ad4cfb9c0d42b061d0e1e6e95c236f5b02ecc02
hawkeye116477 <hawkeye116477@gmail.com>
2020-04-21 11:35:18 +0200

    Bug 1409976 - Part 1: Add support for `slotchange` event.

--- dom/html/HTMLSlotElement.h
+++ dom/html/HTMLSlotElement.h
@@ -63,6 +63,9 @@ public:
   void RemoveAssignedNode(nsINode* aNode);
   void ClearAssignedNodes();
 
+  void EnqueueSlotChangeEvent() const;
+  void FireSlotChangeEvent();
+
 protected:
   virtual ~HTMLSlotElement();
   virtual JSObject*
