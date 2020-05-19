36da62fa7c4a3d41e5e6180aa734541ea036a021
J.C. Jones <jc@mozila.com>
2020-05-19 23:25:11 +0000

    Bug 1632908 - land NSS NSS_3_44_4_RTM UPGRADE_NSS_RELEASE, r=kjacobs a=RyanVM
    2020-05-19  J.C. Jones  <jjones@mozilla.com>
    
    	* lib/nss/nss.h, lib/softoken/softkver.h, lib/util/nssutil.h:
    	Set version numbers to 3.44.4 final
    	[be8b38f87121] [NSS_3_44_4_RTM] <NSS_3_44_BRANCH>
    
    2020-04-23  Robert Relyea  <rrelyea@redhat.com>
    
    	* lib/freebl/dsa.c:
    	Bug 1631576 - Force a fixed length for DSA exponentiation
    	r=pereida,bbrumley
    
    	[48612468b52f] <NSS_3_44_BRANCH>
    
    2020-01-13  Kevin Jacobs  <kjacobs@mozilla.com>
    
    	* automation/taskcluster/windows/build_gyp.sh:
    	Bug 1608895 - Install setuptools<45.0.0 until workers are upgraded
    	to python3 r=jcj
    
    	[[ https://setuptools.readthedocs.io/en/latest/history.html#v45-0-0
    	| Setuptools 45.0.0 ]] drops support for Python2, which our Windows
    	workers are running.
    
    	This patch installs the prior version during build, in order to
    	unblock CI until the workers can be upgraded.
    
    	[42186bde9062] <NSS_3_44_BRANCH>
    
    2019-11-19  J.C. Jones  <jjones@mozilla.com>
    
    	* .hgtags:
    	Added tag NSS_3_44_3_RTM for changeset d871fc63531d
    	[b6910cab5f1e] <NSS_3_44_BRANCH>
    
    Differential Revision: https://phabricator.services.mozilla.com/D76065

--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -19,10 +19,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <Beta>]"
  */
-#define NSSUTIL_VERSION "3.44.3"
+#define NSSUTIL_VERSION "3.44.4"
 #define NSSUTIL_VMAJOR 3
 #define NSSUTIL_VMINOR 44
-#define NSSUTIL_VPATCH 3
+#define NSSUTIL_VPATCH 4
 #define NSSUTIL_VBUILD 0
 #define NSSUTIL_BETA PR_FALSE
 
