045624ea6e27973c4b7a43cb515c24b932956f72
Peacock365 <37863161+Peacock365@users.noreply.github.com>
2020-07-18 21:51:22 +0200

    (Current) Increase session store interval to protect SSDs.
    Waterfox and Firefox write to the SSD quite heavily, thereby impacting their longevity:
    
    https://www.servethehome.com/firefox-is-eating-your-ssd-here-is-how-to-fix-it/
    
    This commit reduces the session recovery saves to a more reasonable once per minute (same as Pale Moon).

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -838,7 +838,7 @@ pref("browser.sessionstore.resume_session_once", false);
 pref("browser.sessionstore.resuming_after_os_restart", false);
 
 // Minimal interval between two save operations in milliseconds (while the user is active).
-pref("browser.sessionstore.interval", 15000); // 15 seconds
+pref("browser.sessionstore.interval", 60000); // 1 minute
 
 // Minimal interval between two save operations in milliseconds (while the user is idle).
 pref("browser.sessionstore.interval.idle", 3600000); // 1h
