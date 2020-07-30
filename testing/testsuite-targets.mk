e3020ae104a442d240d556bc00f504defc3a1520
Mike Hommey <mh+mozilla@glandium.org>
2020-07-30 20:27:14 +0000

    Bug 1614626 - Move xpcshell selftests to before running actual xpcshell tests. r=gbrown, a=RyanVM
    This allows to test them in the exact same environment as the tests are
    going to run, which turns out to have revealed a few issues that would
    only appear once xpcshell tests fail, impeding on debugging those
    failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84781

--- testing/testsuite-targets.mk
+++ testing/testsuite-targets.mk
@@ -260,9 +260,6 @@ stage-extensions: make-stage-dir
 
 check::
 	$(eval cores=$(shell $(PYTHON3) -c 'import multiprocessing; print(multiprocessing.cpu_count())'))
-	@echo "Starting 'mach python-test' with -j$(cores)"
-	@$(topsrcdir)/mach --log-no-times python-test -j$(cores) --subsuite default
-	@echo "Finished 'mach python-test' successfully"
 	@echo "Starting 'mach python-test' with --python $(PYTHON3) -j$(cores)"
 	@$(topsrcdir)/mach --log-no-times python-test --python python3 -j$(cores) --subsuite default
 	@echo "Finished 'mach python-test' with py3 successfully"
