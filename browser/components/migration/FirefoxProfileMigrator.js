9049dd487021b087770bf6e1d8d6b760b3d23646
hawkeye116477 <hawkeye116477@gmail.com>
2020-08-22 14:02:22 +0200

    bug 1394871 - add key4.db to password-related file list in Firefox profile migration code
    NSS stores the password encrypting key in key3.db. When Firefox uses the sqlite-backed NSS databases by default (bug 783994), this file will be called key4.db. The Firefox profile migration code needs to know this.

--- browser/components/migration/FirefoxProfileMigrator.js
+++ browser/components/migration/FirefoxProfileMigrator.js
@@ -131,7 +131,7 @@ FirefoxProfileMigrator.prototype._getResourcesInternal = function(sourceProfileD
   let favicons = getFileResource(types.HISTORY, ["favicons.sqlite", "favicons.sqlite-wal"]);
   let cookies = getFileResource(types.COOKIES, ["cookies.sqlite", "cookies.sqlite-wal"]);
   let passwords = getFileResource(types.PASSWORDS,
-    ["signons.sqlite", "logins.json", "key3.db",
+    ["signons.sqlite", "logins.json", "key3.db", "key4.db",
      "signedInUser.json"]);
   let formData = getFileResource(types.FORMDATA, [
     "formhistory.sqlite",
