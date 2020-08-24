8dd98e984ae8a66022b6e5dd87dffa04e264cc02
Alex Kontos <alex@waterfoxproject.org>
2020-08-24 09:43:42 +0300

    Fix incorrect name.

--- dom/abort/AbortSignal.cpp
+++ dom/abort/AbortSignal.cpp
@@ -60,7 +60,7 @@ AbortSignal::Abort()
     
   // We might be deleted as a result of aborting a follower, so ensure we live
   // until all followers have been aborted.
-  RefPtr<AbortSignalImpl> pinThis = this;
+  RefPtr<AbortSignal> pinThis = this;
 
   // Let's inform the followers.
   for (uint32_t i = 0; i < mFollowers.Length(); ++i) {
