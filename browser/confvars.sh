a3b8137676e80a34a7990640f5753c3f8f1058cd
Alex Kontos <alex@waterfoxproject.org>
2020-09-09 17:59:19 +0300

    Set correct global branding.

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -3,8 +3,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=Firefox
-MOZ_APP_VENDOR=Mozilla
+MOZ_APP_BASENAME=Waterfox
+MOZ_APP_VENDOR=Waterfox Ltd.
 MOZ_UPDATER=1
 
 if test "$OS_ARCH" = "WINNT"; then
@@ -35,8 +35,8 @@ BROWSER_CHROME_URL=chrome://browser/content/browser.xhtml
 # For mozilla-beta, mozilla-release, or mozilla-central repositories, use
 # "unofficial" branding.
 # For the mozilla-aurora repository, use "aurora".
-MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
-MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
+MOZ_BRANDING_DIRECTORY=browser/branding/waterfox
+MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/waterfox
 MOZ_APP_ID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}
 
 MOZ_PROFILE_MIGRATOR=1
