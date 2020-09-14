57d475add1b04f9a7cb156831eac999e73b8b2b7
athenian200 <athenian200@outlook.com>
2020-09-14 21:18:26 +0200

    Bug 1281135 - Part 2: Remove pointless local variables.
    This clarifies the assumptions the code is making and the order in which the variables pass through the loading process. The new variable is set after the sheet is created and prepared, and is assumed to be false in the beginning.
    
    Since the local variable is always initialized to false, we don't actually need to declare it and can just pass "false" directly as a parameter to the PrepareSheet function's bool.

--- layout/style/Loader.cpp
+++ layout/style/Loader.cpp
@@ -2275,7 +2275,6 @@ Loader::LoadChildSheet(StyleSheet* aParentSheet,
     state = eSheetComplete;
   } else {
     bool isAlternate;
-    bool isExplicitlyEnabled;
     const nsAString& empty = EmptyString();
     // For now, use CORS_NONE for child sheets
     rv = CreateSheet(aURL, nullptr, principal,
@@ -2286,7 +2285,9 @@ Loader::LoadChildSheet(StyleSheet* aParentSheet,
                      false, empty, state, &isAlternate, &sheet);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    PrepareSheet(sheet, empty, empty, aMedia, nullptr, isAlternate, isExplicitlyEnabled);
+    // For now, child sheets are not explicitly enabled (seventh argument is
+    // always false here).
+    PrepareSheet(sheet, empty, empty, aMedia, nullptr, isAlternate, false);
   }
 
   rv = InsertChildSheet(sheet, aParentSheet, aGeckoParentRule);
@@ -2413,7 +2414,6 @@ Loader::InternalLoadNonDocumentSheet(nsIURI* aURL,
 
   StyleSheetState state;
   bool isAlternate;
-  bool isExplicitlyEnabled;
   RefPtr<StyleSheet> sheet;
   bool syncLoad = (aObserver == nullptr);
   const nsAString& empty = EmptyString();
@@ -2423,7 +2423,10 @@ Loader::InternalLoadNonDocumentSheet(nsIURI* aURL,
                    false, empty, state, &isAlternate, &sheet);
   NS_ENSURE_SUCCESS(rv, rv);
 
-  PrepareSheet(sheet, empty, empty, nullptr, nullptr, isAlternate, isExplicitlyEnabled);
+  // Sheets can only be explicitly enabled after creation and preparation, so
+  // we always pass false for the initial value of the explicitly enabled flag
+  // when calling PrepareSheet.
+  PrepareSheet(sheet, empty, empty, nullptr, nullptr, isAlternate, false);
 
   if (state == eSheetComplete) {
     LOG(("  Sheet already complete"));
