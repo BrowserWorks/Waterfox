e33d1d49e36b929465840c69123cfa49a16236c7
Andrew Sutherland <asutherland@asutherland.org>
2020-10-28 12:15:23 +0000

    Bug 1669355 - Refactor MIME type warnings into base class. r=valentin, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D95745

--- netwerk/protocol/http/HttpBaseChannel.h
+++ netwerk/protocol/http/HttpBaseChannel.h
@@ -607,6 +607,8 @@ class HttpBaseChannel : public nsHashPropertyBag,
 
   nsresult ComputeCrossOriginOpenerPolicyMismatch();
 
+  nsresult ValidateMIMEType();
+
   friend class PrivateBrowsingChannel<HttpBaseChannel>;
   friend class InterceptFailedOnStop;
 
