d6d2e9c18c5406670a7eb0c8a9905f7316784b33
Mike Hommey <mh+mozilla@glandium.org>
2021-02-17 21:47:28 +0000

    Bug 1670538 - Use an allow-list in RUSTC_BOOTSTRAP for rustc >= 1.50.0. r=firefox-build-system-reviewers,andi,sheehan,mhentges, a=jcristau
    While we could change encoding_rs and packed_simd to not emit
    RUSTC_BOOTSTRAP on newer versions of rust, like we do for gkrust-shared,
    it's not worth the effort (especially because they are vendored).
    
    Differential Revision: https://phabricator.services.mozilla.com/D108098

--- config/makefiles/rust.mk
+++ config/makefiles/rust.mk
@@ -181,6 +181,16 @@ export COREAUDIO_SDK_PATH=$(MACOS_SDK_DIR)
 endif
 endif
 
+ifndef RUSTC_BOOTSTRAP
+ifeq (,$(filter 1.38.% 1.39.% 1.40.% 1.41.% 1.42.% 1.43.% 1.44.% 1.45.% 1.46.% 1.47.% 1.48.% 1.49.%,$(RUSTC_VERSION)))
+RUSTC_BOOTSTRAP := gkrust_shared
+ifdef MOZ_RUST_SIMD
+RUSTC_BOOTSTRAP := $(RUSTC_BOOTSTRAP),encoding_rs,packed_simd
+endif
+export RUSTC_BOOTSTRAP
+endif
+endif
+
 target_rust_ltoable := force-cargo-library-build
 target_rust_nonltoable := force-cargo-test-run force-cargo-library-check $(foreach b,build check,force-cargo-program-$(b))
 
