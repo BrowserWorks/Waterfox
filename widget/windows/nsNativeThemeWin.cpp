1002e8279e0a758eba46745f35a61400e4352187
hawkeye116477 <hawkeye116477@gmail.com>
2021-03-31 23:46:45 +0200

    Bug 1475033 part 6 - Have scrollbar auto colors resolve to the corresponding colors on Windows 10.
    So that we can use custom scrollbar for thin scrollbar.

--- widget/windows/nsNativeThemeWin.cpp
+++ widget/windows/nsNativeThemeWin.cpp
@@ -1920,19 +1920,13 @@ RENDER_AGAIN:
 static nscolor
 GetScrollbarFaceColorForAuto(nsStyleContext* aStyle)
 {
-  // Do we want to derive from scrollbar-track-color when possible?
-  DWORD sysColor = ::GetSysColor(COLOR_SCROLLBAR);
-  return NS_RGB(GetRValue(sysColor),
-                GetGValue(sysColor),
-                GetBValue(sysColor));
+  return NS_RGB(205, 205, 205);
 }
 
 static nscolor
 GetScrollbarTrackColorForAuto(nsStyleContext* aStyle)
 {
-  // Fallback to background color for now. Do we want to derive from
-  // scrollbar-face-color somehow?
-  return aStyle->StyleBackground()->BackgroundColor(aStyle);
+  return NS_RGB(240, 240, 240);
 }
 
 nscolor
