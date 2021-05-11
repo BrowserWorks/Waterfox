35341e3dd8a98440999929f5f412fe316462b7c2
Alex Kontos <MrAlex94@users.noreply.github.com>
2021-05-11 09:52:38 +0100

    Update run.sh

--- build/appveyor/run.sh
+++ build/appveyor/run.sh
@@ -6,7 +6,8 @@ cd /c/Builds/waterfox
 ./mach -v build installer
 
 BROWSER_VERSION=`cat browser/config/version_display.txt`
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe" sign -tr http://timestamp.digicert.com -fd sha256 -f "build/appveyor/waterfox.cer" -csp "eToken Base Cryptographic Provider" -kc "[{{$CSP}}]=te-ba4d65f7-af06-4aa4-91c1-54d4f0cb9b5b" objdir-classic/dist/installer/sea/waterfox-classic-$BROWSER_VERSION.en-US.win64.installer.exe installer/
+ls -a objdir-classic/dist/installer/sea/
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe" sign -tr http://timestamp.digicert.com -fd sha256 -f "build/appveyor/waterfox.cer" -csp "eToken Base Cryptographic Provider" -kc "[{{$CSP}}]=te-ba4d65f7-af06-4aa4-91c1-54d4f0cb9b5b" "objdir-classic/dist/installer/sea/waterfox-classic-$BROWSER_VERSION.en-US.win64.installer.exe"
 
 # YEAR=$(date +%Y)
 # MONTH=$(date +%m)
