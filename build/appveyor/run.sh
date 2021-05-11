edb56a6c9a9237ff0758c13b37330eb60b2cec45
Alex Kontos <MrAlex94@users.noreply.github.com>
2021-05-11 10:57:29 +0100

    Update run.sh

--- build/appveyor/run.sh
+++ build/appveyor/run.sh
@@ -1,13 +1,13 @@
 set -ex
 
-cd /c/Builds/waterfox
+cd $APPVEYOR_BUILD_FOLDER
 
-./mach -v build
-./mach -v build installer
+./mach build
+./mach build installer
 
 BROWSER_VERSION=`cat browser/config/version_display.txt`
-ls -a objdir-classic/dist/installer/sea/
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe" sign -tr http://timestamp.digicert.com -fd sha256 -f "build/appveyor/waterfox.cer" -csp "eToken Base Cryptographic Provider" -kc "[{{$CSP}}]=te-ba4d65f7-af06-4aa4-91c1-54d4f0cb9b5b" "objdir-classic/dist/installer/sea/waterfox-classic-$BROWSER_VERSION.en-US.win64.installer.exe"
+ls -a objdir-classic/dist/install/sea/
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe" sign -tr http://timestamp.digicert.com -fd sha256 -f "build/appveyor/waterfox.cer" -csp "eToken Base Cryptographic Provider" -kc "[{{$CSP}}]=te-ba4d65f7-af06-4aa4-91c1-54d4f0cb9b5b" objdir-classic/dist/install/sea/waterfox-classic-$BROWSER_VERSION.en-US.win64.installer.exe
 
 # YEAR=$(date +%Y)
 # MONTH=$(date +%m)
