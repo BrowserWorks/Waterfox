8163996e76c6fc578468cf3a6ab81125a66006cf
Sebastian Hengst <archaeopteryx@coole-files.de>
2021-05-31 16:08:20 +0200

    Bug 1713497 - work around wpt manifest issue. a=test-only

--- testing/web-platform/manifestdownload.py
+++ testing/web-platform/manifestdownload.py
@@ -78,8 +78,7 @@ def should_download(logger, manifest_paths, rebuild_time=timedelta(days=5)):
 def taskcluster_url(logger, commits):
     artifact_path = '/artifacts/public/manifests.tar.gz'
 
-    repos = {"mozilla-central": "mozilla-central",
-             "integration/autoland": "autoland"}
+    repos = {"releases/mozilla-esr78": "mozilla-esr78"}
     cset_url = ('https://hg.mozilla.org/{repo}/json-pushes?'
                 'changeset={changeset}&version=2&tipsonly=1')
 
