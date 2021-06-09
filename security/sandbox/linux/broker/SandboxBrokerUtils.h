overholt/firefox

Bug 1660901 - Support the fstat-like subset of fstatat in the Linux sandbox policies. r=gcp, a=RyanVM
Differential Revision: https://phabricator.services.mozilla.com/D88499

security/sandbox/linux/broker/SandboxBrokerUtils.h
@@ -19,10 +19,12 @@
 typedef struct stat64 statstruct;
 #  define statsyscall stat64
 #  define lstatsyscall lstat64
+#  define fstatsyscall fstat64
 #elif defined(__NR_stat)
 typedef struct stat statstruct;
 #  define statsyscall stat
 #  define lstatsyscall lstat
+#  define fstatsyscall fstat
 #else
 #  error Missing stat syscall include.
 #endif

BrowserWorks/Waterfox

security/sandbox/linux/broker/SandboxBrokerUtils.h
@@ -19,10 +19,12 @@
 typedef struct stat64 statstruct;
 #define statsyscall stat64
 #define lstatsyscall lstat64
+#define fstatsyscall fstat64
 #elif defined(__NR_stat)
 typedef struct stat statstruct;
 #define statsyscall stat
 #define lstatsyscall lstat
+#define fstatsyscall fstat
 #else
 #error Missing stat syscall include.
 #endif