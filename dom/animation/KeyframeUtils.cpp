384779ecf43ed0d58d4962baa646ab701b4d9f77
hawkeye116477 <hawkeye116477@gmail.com>
2021-06-12 13:47:07 +0200

    Bug 1471814 - Ignore the composite member on Keyframe objects when the compositing pref is not set.

--- dom/animation/KeyframeUtils.cpp
+++ dom/animation/KeyframeUtils.cpp
@@ -669,7 +669,8 @@ ConvertKeyframeSequence(JSContext* aCx,
       keyframe->mOffset.emplace(keyframeDict.mOffset.Value());
     }
 
-    if (!keyframeDict.mComposite.IsNull()) {
+    if (Preferences::GetBool("dom.animations-api.compositing.enabled") &&
+        !keyframeDict.mComposite.IsNull()) {
       keyframe->mComposite.emplace(keyframeDict.mComposite.Value());
     }
 
@@ -1541,26 +1542,28 @@ GetKeyframeListFromPropertyIndexedKeyframe(JSContext* aCx,
   //
   // This corresponds to step 5, "Otherwise," branch, substep 12 of
   // https://drafts.csswg.org/web-animations/#processing-a-keyframes-argument
-  const FallibleTArray<Nullable<dom::CompositeOperation>>* compositeOps =
-    nullptr;
-  AutoTArray<Nullable<dom::CompositeOperation>, 1> singleCompositeOp;
-  auto& composite = keyframeDict.mComposite;
-  if (composite.IsCompositeOperation()) {
-    singleCompositeOp.AppendElement(composite.GetAsCompositeOperation());
-    const FallibleTArray<Nullable<dom::CompositeOperation>>& asFallibleArray =
-      singleCompositeOp;
-    compositeOps = &asFallibleArray;
-  } else if (composite.IsCompositeOperationOrNullSequence()) {
-    compositeOps = &composite.GetAsCompositeOperationOrNullSequence();
-  }
+  if (Preferences::GetBool("dom.animations-api.compositing.enabled")) {
+    const FallibleTArray<Nullable<dom::CompositeOperation>>* compositeOps =
+      nullptr;
+    AutoTArray<Nullable<dom::CompositeOperation>, 1> singleCompositeOp;
+    auto& composite = keyframeDict.mComposite;
+    if (composite.IsCompositeOperation()) {
+      singleCompositeOp.AppendElement(composite.GetAsCompositeOperation());
+      const FallibleTArray<Nullable<dom::CompositeOperation>>& asFallibleArray =
+        singleCompositeOp;
+      compositeOps = &asFallibleArray;
+    } else if (composite.IsCompositeOperationOrNullSequence()) {
+      compositeOps = &composite.GetAsCompositeOperationOrNullSequence();
+    }
 
-  // Fill in and repeat as needed.
-  if (compositeOps && !compositeOps->IsEmpty()) {
-    size_t length = compositeOps->Length();
-    for (size_t i = 0; i < aResult.Length(); i++) {
-      if (!compositeOps->ElementAt(i % length).IsNull()) {
-        aResult[i].mComposite.emplace(
-          compositeOps->ElementAt(i % length).Value());
+    // Fill in and repeat as needed.
+    if (compositeOps && !compositeOps->IsEmpty()) {
+      size_t length = compositeOps->Length();
+      for (size_t i = 0; i < aResult.Length(); i++) {
+        if (!compositeOps->ElementAt(i % length).IsNull()) {
+          aResult[i].mComposite.emplace(
+            compositeOps->ElementAt(i % length).Value());
+        }
       }
     }
   }
