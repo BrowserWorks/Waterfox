204be36df565206be3929ddbe3c1fa8033434fd2
hawkeye116477 <hawkeye116477@gmail.com>
2021-07-19 17:14:34 +0200

    Bug 1500733 - Remove canvas.imagebitmap_extensions.enabled preference.

--- dom/canvas/ImageBitmap.cpp
+++ dom/canvas/ImageBitmap.cpp
@@ -1515,17 +1515,5 @@ ImageBitmap::WriteStructuredClone(JSStructuredCloneWriter* aWriter,
   return true;
 }
 
-/*static*/ bool
-ImageBitmap::ExtensionsEnabled(JSContext* aCx, JSObject*)
-{
-  if (NS_IsMainThread()) {
-    return Preferences::GetBool("canvas.imagebitmap_extensions.enabled");
-  } else {
-    WorkerPrivate* workerPrivate = GetWorkerPrivateFromContext(aCx);
-    MOZ_ASSERT(workerPrivate);
-    return workerPrivate->ImageBitmapExtensionsEnabled();
-  }
-}
-
 } // namespace dom
 } // namespace mozilla
