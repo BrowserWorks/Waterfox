459c813004193d6fdea14c95b03594e386c47c18
Daniel Holbert <dholbert@cs.stanford.edu>
2021-08-06 18:46:22 +0000

    Bug 1672640 part 3: Adjust GetNaturalBaselineBOffset implementations to bail (report no baseline) if the frame has 'contain:layout'. r=TYLin, a=RyanVM
    Some of our GetNaturalBaselineBOffset implementations already have this; others
    don't.  But they all should have it, or else a caller might improperly query
    their baseline and use it for layout despite the frame having 'contain:layout'.
    
    Without this patch, the rest of this patch-stack makes us fail WPT test
    contain-layout-suppress-baseline-002.html because we improperly honor the
    baseline for the 'contain:layout' buttons at the top of the test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121938

--- layout/tables/nsTableFrame.cpp
+++ layout/tables/nsTableFrame.cpp
@@ -3641,6 +3641,10 @@ nscoord nsTableFrame::GetLogicalBaseline(WritingMode aWM) const {
 bool nsTableFrame::GetNaturalBaselineBOffset(
     WritingMode aWM, BaselineSharingGroup aBaselineGroup,
     nscoord* aBaseline) const {
+  if (StyleDisplay()->IsContainLayout()) {
+    return false;
+  }
+
   RowGroupArray orderedRowGroups;
   OrderRowGroups(orderedRowGroups);
   // XXX not sure if this should be the size of the containing block instead.
