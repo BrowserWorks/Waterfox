a8867e93c49c9931278082fe89f32d38e46679e5
Alex Kontos <alex@waterfoxproject.org>
2021-09-17 11:16:06 +0100

    [WFX-199] Stop building pingsender executable.

--- toolkit/components/telemetry/app/TelemetrySend.sys.mjs
+++ toolkit/components/telemetry/app/TelemetrySend.sys.mjs
@@ -1656,46 +1656,6 @@ export var TelemetrySendImpl = {
   },
 
   runPingSender(pings, observer) {
-    if (AppConstants.platform === "android") {
-      throw Components.Exception("", Cr.NS_ERROR_NOT_IMPLEMENTED);
-    }
-
-    let suppressPingsender = Services.prefs.getBoolPref(
-      "toolkit.telemetry.testing.suppressPingsender",
-      false
-    );
-    if (suppressPingsender) {
-      this._log.trace("Silently skipping pingsender call in automation");
-      return;
-    }
-
-    // By default, invoke `pingsender[.exe] URL path ...`.
-    let exeName =
-      AppConstants.platform === "win" ? "pingsender.exe" : "pingsender";
-    let params = [];
-
-    if (lazy.NimbusFeatures.pingsender.getVariable("backgroundTaskEnabled")) {
-      // If using pingsender background task, invoke `firefox[.exe] --backgroundtask pingsender URL path ...`.
-      exeName =
-        AppConstants.MOZ_APP_NAME +
-        (AppConstants.platform === "win" ? ".exe" : "");
-      params = ["--backgroundtask", "pingsender"];
-    }
-
-    this._log.info(
-      `Invoking '${exeName}${params.length ? " " + params.join(" ") : ""} ...'`
-    );
-
-    let exe = Services.dirsvc.get("GreBinD", Ci.nsIFile);
-    exe.append(exeName);
-
-    params.push(...pings.flatMap(ping => [ping.url, ping.path]));
-    let process = Cc["@mozilla.org/process/util;1"].createInstance(
-      Ci.nsIProcess
-    );
-    process.init(exe);
-    process.startHidden = true;
-    process.noShell = true;
-    process.runAsync(params, params.length, observer);
+    throw Components.Exception("", Cr.NS_ERROR_NOT_IMPLEMENTED);
   },
 };
