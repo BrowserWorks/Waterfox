a05af573e387063a4e13562e14bf948ffe2c2a03
adamp01 <47503375+adamp01@users.noreply.github.com>
2022-01-07 16:01:31 +0000

    [WFX-162] Increase test coverage for extensibles.

--- browser/extensions/extensibles/test/browser/browser_tabfeatures.js
+++ browser/extensions/extensibles/test/browser/browser_tabfeatures.js
@@ -7,18 +7,18 @@ add_task(async function testCopyTabUrls() {
   ok(copyTabUrl, "Copy tab URL is included");
   ok(copyAllTabUrls, "Copy all tab URLs is included");
   // Make sure that defaults are set correctly
-  let contextMenu = await openAndCloseTabContextMenu(gBrowser.selectedTab);
+  await openAndCloseTabContextMenu(gBrowser.selectedTab);
   is(copyTabUrl.hidden, false, "Copy tab URL visible by default");
   is(copyAllTabUrls.hidden, true, "Copy all tab URLs hidden by default");
   // Make sure changing prefs causes elements to be shown/hidden
   Services.prefs.setBoolPref(COPY_URL_PREF, false);
   Services.prefs.setBoolPref(COPY_ALL_URLS_PREF, false);
-  contextMenu = await openAndCloseTabContextMenu(gBrowser.selectedTab);
+  await openAndCloseTabContextMenu(gBrowser.selectedTab);
   is(copyTabUrl.hidden, true, "Copy tab URL hidden");
   is(copyAllTabUrls.hidden, true, "Copy all tab URLs hidden");
   Services.prefs.setBoolPref(COPY_URL_PREF, true);
   Services.prefs.setBoolPref(COPY_ALL_URLS_PREF, true);
-  contextMenu = await openAndCloseTabContextMenu(gBrowser.selectedTab);
+  await openAndCloseTabContextMenu(gBrowser.selectedTab);
   is(copyTabUrl.hidden, false, "Copy tab URL visible");
   is(copyAllTabUrls.hidden, false, "Copy all tab URLs visible");
   Services.prefs.clearUserPref(COPY_URL_PREF);
@@ -33,12 +33,12 @@ add_task(async function testHideDuplicateTab() {
   // Setting duplicateTab pref to false should hide element in all windows
   let duplicateTab = document.getElementById("context_duplicateTab");
   Services.prefs.setBoolPref(DUPLICATE_TAB_PREF, false);
-  let contextMenu = await openAndCloseTabContextMenu(gBrowser.selectedTab);
+  await openAndCloseTabContextMenu(gBrowser.selectedTab);
   is(duplicateTab.hidden, true, "Duplicate tab hidden");
   // Should fall back to default value of true, i.e. element showing
   Services.prefs.clearUserPref(DUPLICATE_TAB_PREF);
   // Ensure showing
-  contextMenu = await openAndCloseTabContextMenu(gBrowser.selectedTab);
+  await openAndCloseTabContextMenu(gBrowser.selectedTab);
   is(duplicateTab.hidden, false, "Duplicate tab showing");
 });
 
@@ -79,9 +79,64 @@ add_task(async function testRestartItem() {
   // TODO: Test purgecache clears cache
 });
 
-add_task(async function testMoveTabBar() {});
-// TODO: Move bookmarks bar
-// TODO: Style buttonbox and menubar (Not on Mac)
+add_task(async function testMoveTabBar() {
+  // Test default (topabove)
+  let el = document.querySelector("#TabsToolbar");
+  let bottomBox = document.querySelector("#browser-bottombox");
+  is(el.parentElement.id, "titlebar", "Tab toolbar is below menu bar");
+  // Test topbelow
+  Services.prefs.setCharPref(TABBAR_POSITION_PREF, "topbelow");
+  is(el.parentElement.id, "navigator-toolbox", "Tabs toolbar is below nav bar");
+  // Test bottom above
+  Services.prefs.setCharPref(TABBAR_POSITION_PREF, "bottomabove");
+  is(el.parentElement.id, "browser-bottombox", "Tabs toolbar is in bottom box");
+  is(
+    bottomBox.firstChild.id,
+    "TabsToolbar",
+    "Tabs toolbar is first element in bottom box"
+  );
+  // Test bottom below (reliance upon StatusBar.jsm)
+  Services.prefs.setCharPref(TABBAR_POSITION_PREF, "bottombelow");
+  is(el.parentElement.id, "browser-bottombox", "Tabs toolbar is in bottom box");
+  is(
+    bottomBox.firstChild.id,
+    "status-bar",
+    "Tabs toolbar is not first element in bottom box"
+  );
+  // Clear pref
+  Services.prefs.clearUserPref(TABBAR_POSITION_PREF);
+});
 
-// TODO: PrivateTab
-// TODO: StatusBar
+add_task(async function testMoveBookmarksBar() {
+  // Test default (top)
+  let el = document.querySelector("#PersonalToolbar");
+  is(
+    el.parentElement.id,
+    "navigator-toolbox",
+    "Bookmarks bar is below nav bar"
+  );
+  // Test bottom
+  Services.prefs.setCharPref(BOOKMARKBAR_POSITION_PREF, "bottom");
+  is(
+    el.parentElement.id,
+    "browser-bottombox",
+    "Bookmarks bar is in bottom box"
+  );
+  // Test bottom buttons not disabled
+  // Test bottom context menu item still visible
+  // Clear pref
+  Services.prefs.clearUserPref(BOOKMARKBAR_POSITION_PREF);
+});
+
+add_task(async function testWindowButtons() {
+  // Test menu bar visible and tabs in default position
+  // Test menu bar visible and tabs not in default position
+  // Test menu bar hidden and tabs in default position
+  // Test menu bar hidden and tabs not in default position
+});
+
+add_task(async function testSecondWindow() {
+  // Change all prefs to non default values
+  // Open new browser window
+  // Test all cases reflected in new window
+});
