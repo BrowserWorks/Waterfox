b1310affe918853e8bf7affdf0f7effbf823c627
adamp01 <47503375+adamp01@users.noreply.github.com>
2022-03-10 11:12:37 +0000

    [WFX-356] Add addon version to about:addons list view.

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -3463,6 +3463,10 @@ class AddonCard extends HTMLElement {
     }
     nameContainer.prepend(nameHeading);
 
+    // WATERFOX
+    let collapsedVersion = this.card.querySelector(".addon-version-number");
+    collapsedVersion.textContent = addon.version;
+
     let panelType = addon.type == "plugin" ? "plugin-options" : "addon-options";
     this.options = document.createElement(panelType);
     this.options.render();
