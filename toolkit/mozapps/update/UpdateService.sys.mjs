b2f2a2cfff6e050601977c0c84aceb28f0ed716a
Alex Kontos <MrAlex94@users.noreply.github.com>
2022-07-14 15:43:17 +0100

    [WFX-344] Add update check disabling.

--- toolkit/mozapps/update/UpdateService.sys.mjs
+++ toolkit/mozapps/update/UpdateService.sys.mjs
@@ -3872,6 +3872,15 @@ UpdateService.prototype = {
    * See nsIUpdateService.idl
    */
   get canUsuallyCheckForUpdates() {
+    let prefEnabled = Services.prefs.getBoolPref("app.update.enabled", true);
+    if (!prefEnabled) {
+      LOG(
+        "UpdateService.canUsuallyCheckForUpdates - unable to automatically check " +
+          "the preference is disabled."
+      );
+      return false;
+    }
+
     if (this.disabled) {
       LOG(
         "UpdateService.canUsuallyCheckForUpdates - unable to automatically check " +
