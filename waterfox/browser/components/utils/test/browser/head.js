ad0aa8324f27a3652f8867418a88e06bb034b7d8
adamp01 <adamwoodce@gmail.com>
2022-07-21 08:51:59 +0100

    [WFX-401] Add utility modules.

---
+++ waterfox/browser/components/utils/test/browser/head.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const { PrefUtils } = ChromeUtils.import("resource:///modules/PrefUtils.jsm");
+
+const STRING_PREF = "browser.test.stringPref";
+const INT_PREF = "browser.test.intPref";
+const BOOL_PREF = "browser.test.boolPref";
