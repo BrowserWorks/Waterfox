d6010553c58ac1775321276db9365f34f36a6301
Alex Kontos <MrAlex94@users.noreply.github.com>
2022-09-16 15:41:24 +0100

    [WFX-202] Rename Firefox related references to their relevant counterparts.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -1681,12 +1681,12 @@ BrowserGlue.prototype = {
       if (updateChannel) {
         let uninstalledValue = lazy.WindowsRegistry.readRegKey(
           Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
-          "Software\\Mozilla\\Firefox",
+          "Software\\WaterfoxLimited\\Waterfox",
           `Uninstalled-${updateChannel}`
         );
         let removalSuccessful = lazy.WindowsRegistry.removeRegKey(
           Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
-          "Software\\Mozilla\\Firefox",
+          "Software\\WaterfoxLimited\\Waterfox",
           `Uninstalled-${updateChannel}`
         );
         if (removalSuccessful && uninstalledValue == "True") {
