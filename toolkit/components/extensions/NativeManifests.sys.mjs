5bb6f96e8df7e7105cbd57286ba34f125678e3c2
Alex Kontos <MrAlex94@users.noreply.github.com>
2022-09-16 15:41:24 +0100

    [WFX-202] Rename Firefox related references to their relevant counterparts.

--- toolkit/components/extensions/NativeManifests.sys.mjs
+++ toolkit/components/extensions/NativeManifests.sys.mjs
@@ -25,6 +25,10 @@ const TYPES = {
 const NATIVE_MANIFEST_SCHEMA =
   "chrome://extensions/content/schemas/native_manifest.json";
 
+// WATERFOX
+// Future rebase take note: leave this so extensions
+// can communicate with software without being
+// made specifically for Waterfox.
 const REGPATH = "Software\\Mozilla";
 
 export var NativeManifests = {
