7984986e837482bd45ef81f1dd7356475833e70d
adamp01 <47503375+adamp01@users.noreply.github.com>
2022-09-27 09:28:42 +0100

    [WFX-485] Remove 'More From Mozilla' with pref. (#2808)

--- waterfox/browser/app/profile/000-waterfox.js
+++ waterfox/browser/app/profile/000-waterfox.js
@@ -243,6 +243,7 @@ pref("userContent.page.dark_mode",        true); // Need proton_color
 pref("userContent.page.proton",           true); // Need proton_color
 
 // -- Hide promos -------------------------------------------------------------
+pref("browser.preferences.moreFromMozilla", false, locked);
 pref("browser.vpn_promo.enabled", false, locked);
 pref("browser.promo.focus.enabled", false, locked);
 pref("browser.contentblocking.report.lockwise.enabled", false, locked);
