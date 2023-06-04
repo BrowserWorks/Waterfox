Floorp-Projects/Floorp

Bug 1778997 - provide pkcs11 rust bindings in-tree r=jschanck,supply-chain-reviewers
Differential Revision: https://phabricator.services.mozilla.com/D154258

security/manager/ssl/osclientcerts/src/lib.rs
@@ -16,14 +16,14 @@ extern crate lazy_static;
 extern crate libloading;
 #[macro_use]
 extern crate log;
-extern crate pkcs11;
+extern crate pkcs11_bindings;
 #[macro_use]
 extern crate rsclientcerts;
 extern crate sha2;
 #[cfg(target_os = "windows")]
 extern crate winapi;
 
-use pkcs11::types::*;
+use pkcs11_bindings::*;
 use rsclientcerts::manager::{ManagerProxy, SlotType};
 use std::sync::Mutex;
 use std::thread;
@@ -91,7 +91,7 @@ macro_rules! log_with_thread_id {
 
 /// This gets called to initialize the module. For this implementation, this consists of
 /// instantiating the `ManagerProxy`.
-extern "C" fn C_Initialize(_pInitArgs: CK_C_INITIALIZE_ARGS_PTR) -> CK_RV {
+extern "C" fn C_Initialize(_pInitArgs: CK_VOID_PTR) -> CK_RV {
     // This will fail if this has already been called, but this isn't a problem because either way,
     // logging has been initialized.
     let _ = env_logger::try_init();
@@ -500,7 +500,7 @@ extern "C" fn C_GetAttributeValue(
     let mut attr_types = Vec::with_capacity(ulCount as usize);
     for i in 0..ulCount as usize {
         let attr = unsafe { &*pTemplate.add(i) };
-        attr_types.push(attr.attrType);
+        attr_types.push(attr.type_);
     }
     let mut manager_guard = try_to_get_manager_guard!();
     let manager = manager_guard_to_manager!(manager_guard);
@@ -552,7 +552,7 @@ extern "C" fn C_SetAttributeValue(
 }
 
 fn trace_attr(prefix: &str, attr: &CK_ATTRIBUTE) {
-    let typ = match unsafe_packed_field_access!(attr.attrType) {
+    let typ = match unsafe_packed_field_access!(attr.type_) {
         CKA_CLASS => "CKA_CLASS".to_string(),
         CKA_TOKEN => "CKA_TOKEN".to_string(),
         CKA_LABEL => "CKA_LABEL".to_string(),
@@ -565,13 +565,13 @@ fn trace_attr(prefix: &str, attr: &CK_ATTRIBUTE) {
         CKA_KEY_TYPE => "CKA_KEY_TYPE".to_string(),
         CKA_MODULUS => "CKA_MODULUS".to_string(),
         CKA_EC_PARAMS => "CKA_EC_PARAMS".to_string(),
-        _ => format!("0x{:x}", unsafe_packed_field_access!(attr.attrType)),
+        _ => format!("0x{:x}", unsafe_packed_field_access!(attr.type_)),
     };
     let value =
         unsafe { std::slice::from_raw_parts(attr.pValue as *const u8, attr.ulValueLen as usize) };
     log_with_thread_id!(
         trace,
-        "{}CK_ATTRIBUTE {{ attrType: {}, pValue: {:?}, ulValueLen: {} }}",
+        "{}CK_ATTRIBUTE {{ type: {}, pValue: {:?}, ulValueLen: {} }}",
         prefix,
         typ,
         value,
@@ -599,7 +599,7 @@ extern "C" fn C_FindObjectsInit(
         let slice = unsafe {
             std::slice::from_raw_parts(attr.pValue as *const u8, attr.ulValueLen as usize)
         };
-        attrs.push((attr.attrType, slice.to_owned()));
+        attrs.push((attr.type_, slice.to_owned()));
     }
     let mut manager_guard = try_to_get_manager_guard!();
     let manager = manager_guard_to_manager!(manager_guard);

BrowserWorks/Waterfox

security/manager/ssl/osclientcerts/src/lib.rs
@@ -16,14 +16,14 @@ extern crate lazy_static;
 extern crate libloading;
 #[macro_use]
 extern crate log;
-extern crate pkcs11;
+extern crate pkcs11_bindings;
 #[macro_use]
 extern crate rsclientcerts;
 extern crate sha2;
 #[cfg(target_os = "windows")]
 extern crate winapi;
 
-use pkcs11::types::*;
+use pkcs11_bindings::*;
 use rsclientcerts::manager::{ManagerProxy, SlotType};
 use std::ffi::CStr;
 use std::sync::Mutex;
@@ -109,7 +109,7 @@ macro_rules! log_with_thread_id {
 
 /// This gets called to initialize the module. For this implementation, this consists of
 /// instantiating the `ManagerProxy`.
-extern "C" fn C_Initialize(pInitArgs: CK_C_INITIALIZE_ARGS_PTR) -> CK_RV {
+extern "C" fn C_Initialize(pInitArgs: CK_VOID_PTR) -> CK_RV {
     // This will fail if this has already been called, but this isn't a problem because either way,
     // logging has been initialized.
     let _ = env_logger::try_init();
@@ -516,7 +516,7 @@ extern "C" fn C_GetAttributeValue(
     let mut attr_types = Vec::with_capacity(ulCount as usize);
     for i in 0..ulCount as usize {
         let attr = unsafe { &*pTemplate.add(i) };
-        attr_types.push(attr.attrType);
+        attr_types.push(attr.type_);
     }
     let mut module_state_guard = try_to_get_module_state_guard!();
     let manager = module_state_guard_to_manager!(module_state_guard);
@@ -568,7 +568,7 @@ extern "C" fn C_SetAttributeValue(
 }
 
 fn trace_attr(prefix: &str, attr: &CK_ATTRIBUTE) {
-    let typ = match unsafe_packed_field_access!(attr.attrType) {
+    let typ = match unsafe_packed_field_access!(attr.type_) {
         CKA_CLASS => "CKA_CLASS".to_string(),
         CKA_TOKEN => "CKA_TOKEN".to_string(),
         CKA_LABEL => "CKA_LABEL".to_string(),
@@ -581,13 +581,13 @@ fn trace_attr(prefix: &str, attr: &CK_ATTRIBUTE) {
         CKA_KEY_TYPE => "CKA_KEY_TYPE".to_string(),
         CKA_MODULUS => "CKA_MODULUS".to_string(),
         CKA_EC_PARAMS => "CKA_EC_PARAMS".to_string(),
-        _ => format!("0x{:x}", unsafe_packed_field_access!(attr.attrType)),
+        _ => format!("0x{:x}", unsafe_packed_field_access!(attr.type_)),
     };
     let value =
         unsafe { std::slice::from_raw_parts(attr.pValue as *const u8, attr.ulValueLen as usize) };
     log_with_thread_id!(
         trace,
-        "{}CK_ATTRIBUTE {{ attrType: {}, pValue: {:?}, ulValueLen: {} }}",
+        "{}CK_ATTRIBUTE {{ type: {}, pValue: {:?}, ulValueLen: {} }}",
         prefix,
         typ,
         value,
@@ -615,7 +615,7 @@ extern "C" fn C_FindObjectsInit(
         let slice = unsafe {
             std::slice::from_raw_parts(attr.pValue as *const u8, attr.ulValueLen as usize)
         };
-        attrs.push((attr.attrType, slice.to_owned()));
+        attrs.push((attr.type_, slice.to_owned()));
     }
     let mut module_state_guard = try_to_get_module_state_guard!();
     let manager = module_state_guard_to_manager!(module_state_guard);