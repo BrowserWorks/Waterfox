2445c0ef2f01b873a40cfaeb230c90a982a13df7
goodusername123 <61405538+goodusername123@users.noreply.github.com>
2023-11-17 11:52:25 -0600

    Trust binaries signed for Waterfox
    This fixes about:third-party not trusting any of Waterfox's dll files.

--- toolkit/xre/dllservices/ModuleEvaluator.cpp
+++ toolkit/xre/dllservices/ModuleEvaluator.cpp
@@ -174,6 +174,8 @@ Maybe<ModuleTrustFlags> ModuleEvaluator::GetTrust(
       return Some(ModuleTrustFlags::MicrosoftWindowsSignature);
     } else if (signedBy.EqualsLiteral("Mozilla Corporation")) {
       return Some(ModuleTrustFlags::MozillaSignature);
+    } else if (signedBy.EqualsLiteral("BrowserWorks Ltd")) {
+      return Some(ModuleTrustFlags::MozillaSignature);
     } else {
       // Being signed by somebody who is neither Microsoft nor us is an
       // automatic and immediate disqualification.
