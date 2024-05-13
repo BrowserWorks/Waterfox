7c008501b1f183f2a8919b73c89025d591cbb842
Alex Kontos <MrAlex94@users.noreply.github.com>
2024-05-13 13:43:57 +0100

    Update Sidebar to 1.0.0.2.
    * Keep sidebar showing after leaving  fullscreen mode.
    * Respect user's choice about browser.tabs.insertAfterCurrent and browser.tabs.insertRelatedAfterCurrent

--- waterfox/browser/components/sidebar/background/handle-new-tabs.js
+++ waterfox/browser/components/sidebar/background/handle-new-tabs.js
@@ -528,7 +528,10 @@ Tab.onUpdated.addListener((tab, changeInfo) => {
     const siteMatcher  = /^\w+:\/\/([^\/]+)(?:$|\/.*$)/;
     const openerTabSite = possibleOpenerTab.url.match(siteMatcher);
     const newTabSite    = tab.url.match(siteMatcher);
-    if (openerTabSite && newTabSite && openerTabSite[1] == newTabSite[1]) {
+    if (openerTabSite &&
+        newTabSite &&
+        tab.url != possibleOpenerTab.url && // It may be opened by "Duplciate Tab" or "Open in New Container Tab" if the URL is completely same.
+        openerTabSite[1] == newTabSite[1]) {
       log('behave as a tab opened from same site (delayed)');
       tab.$TST.addState(Constants.kTAB_STATE_OPENED_FOR_SAME_WEBSITE);
       handleNewTabFromActiveTab(tab, {
