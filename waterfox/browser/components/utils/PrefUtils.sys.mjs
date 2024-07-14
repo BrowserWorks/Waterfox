9b756f0be8124dce842e21029a5922c5acc5bb4b
Alex Kontos <MrAlex94@users.noreply.github.com>
2024-07-14 11:56:34 +0100

    Remove imports for `Services` due to property being available in all privileged scopes.
    See: Bug 1667455.

--- waterfox/browser/components/utils/PrefUtils.sys.mjs
+++ waterfox/browser/components/utils/PrefUtils.sys.mjs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-
 export var PrefUtils = {
   get(prefPath, valueIfUndefined, def = false, setDefault = true) {
     let sPrefs = def ? Services.prefs.getDefaultBranch(null) : Services.prefs;
