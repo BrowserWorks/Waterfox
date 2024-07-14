380e6feb6752a97c3e87018945ab570eba7f9846
Alex Kontos <MrAlex94@users.noreply.github.com>
2024-07-14 11:56:34 +0100

    Remove imports for `Services` due to property being available in all privileged scopes.
    See: Bug 1667455.

--- waterfox/browser/extensions/common/ChromeManifest.sys.mjs
+++ waterfox/browser/extensions/common/ChromeManifest.sys.mjs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-
 /**
  * A parser for chrome.manifest files. Implements a subset of
  * https://developer.mozilla.org/en-US/docs/Mozilla/Chrome_Registration
