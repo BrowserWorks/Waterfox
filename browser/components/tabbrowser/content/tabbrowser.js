998338ef82758a0d1ee07e3961cb2dd1eeba1037
goodusername123 <61405538+goodusername123@users.noreply.github.com>
2024-10-13 03:26:27 -0500

    Restore "unread" tab Attribute
    This restore the `unread` tab Attribute that was removed all the way back in Firefox 61.
    This is useful for theming since it allows custom CSS styling for tabs that has been opened in the background but haven't been actually viewed yet.
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1453957

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -1312,6 +1312,7 @@
       if (!this._previewMode) {
         newTab.recordTimeFromUnloadToReload();
         newTab.updateLastAccessed();
+        newTab.removeAttribute("unread");
         oldTab.updateLastAccessed();
         // if this is the foreground window, update the last-seen timestamps.
         if (this.ownerGlobal == BrowserWindowTracker.getTopWindow()) {
@@ -6997,6 +6998,9 @@
 
             this.mTab.setAttribute("bursting", "true");
           }
+
+          if (!this.mTab.selected)
+            this.mTab.setAttribute("unread", "true");
         }
 
         if (this.mTab.hasAttribute("progress")) {
