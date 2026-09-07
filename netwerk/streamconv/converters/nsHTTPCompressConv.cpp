62745e8e45f39145368e0510e9b7a7b253873dd6
Randell Jesup <rjesup@mozilla.com>
2026-09-07 19:54:29 +0000

    Bug 2068378 - Dispatch pass-through OnDataAvailable to the main thread in nsHTTPCompressConv  a=dmeehan DONTBUILD
    Read the data and hand it to do_OnDataAvailable() when a bounce is
    needed; keep the direct pass-through otherwise.
    
    Original Revision: https://phabricator.services.mozilla.com/D322964
    
    Differential Revision: https://phabricator.services.mozilla.com/D323958

--- netwerk/streamconv/converters/nsHTTPCompressConv.cpp
+++ netwerk/streamconv/converters/nsHTTPCompressConv.cpp
@@ -14,6 +14,7 @@
 #include "nsIThreadRetargetableRequest.h"
 #include "nsIThreadRetargetableStreamListener.h"
 #include "nsThreadUtils.h"
+#include "nsNetUtil.h"
 #include "nsStreamUtils.h"
 #include "nsStringStream.h"
 #include "nsComponentManagerUtils.h"
@@ -945,7 +946,18 @@ nsHTTPCompressConv::OnDataAvailable(nsIRequest* request, nsIInputStream* iStr,
       }
     } break;
 
-    default:
+    default: {
+      // Pass-through modes must honour mDispatchToMainThread as well.
+      // CheckListenerChain() reports success for chains that can only run on
+      // the main thread, on the promise that this converter bounces the data
+      // back there.
+      if (mDispatchToMainThread && !NS_IsMainThread()) {
+        nsAutoCString data;
+        MOZ_TRY(NS_ReadInputStreamToString(iStr, data, streamLen));
+        return do_OnDataAvailable(request, aSourceOffset, data.BeginReading(),
+                                  data.Length());
+      }
+
       nsCOMPtr<nsIStreamListener> listener;
       {
         MutexAutoLock lock(mMutex);
@@ -955,6 +967,7 @@ nsHTTPCompressConv::OnDataAvailable(nsIRequest* request, nsIInputStream* iStr,
       if (NS_FAILED(rv)) {
         return rv;
       }
+    } break;
   } /* switch */
 
   return NS_OK;
